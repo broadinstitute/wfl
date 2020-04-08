99cb8172f7c3a3ea013bc6123da0bc273ffeb977
Rex Wang <rexwangcc@gmail.com>
2020-04-07 20:20:58 -0400

    Implement the functions to generate credentials from service accounts and cache.

--- src/zero/once.clj
+++ src/zero/once.clj
@@ -1,9 +1,12 @@
 (ns zero.once
   "Low-level vars to define exactly once and auth related junk."
   (:require [clojure.data.json  :as json]
-            [zero.util          :as util])
+            [zero.util          :as util]
+            [zero.environments  :as env])
   (:import [com.google.auth.oauth2 UserCredentials]
-           [java.net URI]))
+           [java.net URI]
+           (java.io FileInputStream)
+           (com.google.auth.oauth2 GoogleCredentials)))
 
 ;; https://developers.google.com/identity/protocols/OAuth2InstalledApp#refresh
 ;;
@@ -31,3 +34,37 @@
   under the hood if necessary."
   []
   (util/bearer-token-header-for @the-cached-credentials))
+
+(defn new-credentials-from-service-account
+  "Generate scoped GoogleCredentials from a service account FILE."
+  [^String file]
+  (when-not (nil? file)
+    (let [scopes ["email" "profile" "openid"]
+          credentials (GoogleCredentials/fromStream (FileInputStream. file))]
+      (.createScoped credentials scopes))))
+
+(defn service-account-per-env
+  "Map of environment to service account path."
+  []
+  (letfn [(get-sa [environment] {(keyword environment)
+                                 (get-in env/stuff [(keyword environment) :project-service-account])})]
+    (into {} (map get-sa env/registered-envs))))
+
+(defn update-map-vals
+  "Map F over every value of M, with the keys remain the same."
+  [f m]
+  (reduce-kv (fn [m k v] (assoc m k (f v))) {} m))
+
+;; The re-usable environment:credentials-object map for token generation and refreshing
+;;
+(defonce the-cached-credentials-from-service-account
+         (delay (update-map-vals new-credentials-from-service-account (service-account-per-env))))
+
+(comment
+  (some-> (:xx @the-cached-credentials-from-service-account)
+          .refreshAccessToken
+          .getTokenValue)
+  (let [envs {:wgs-dev "some-test-service-account.json"
+              :xx      "some-test-service-account.json"}]
+    (update-map-vals new-credentials-from-service-account envs))
+  )
