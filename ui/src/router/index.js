1ddfb308b6e172997c5c5f7d61eb3de9516fcc6f
Rex Wang <rexwangcc@gmail.com>
2020-05-01 13:02:11 -0400

    Address comment.

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -14,7 +14,7 @@ const routes = [
     path: '/query',
     name: 'query',
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (query.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "query" */ '../views/Query.vue')
   },
@@ -22,7 +22,7 @@ const routes = [
     path: '/workload',
     name: 'workload',
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (workload.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "workload" */ '../views/Workload.vue')
   },
@@ -30,7 +30,7 @@ const routes = [
     path: '/modules',
     name: 'modules',
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (modules.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "modules" */ '../views/Modules.vue')
   }
