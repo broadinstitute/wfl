9bf85eac7616e0ed35e434063e133687c4331fa0
Tom Lyons <tbl@broadinstitute.org>
2020-05-29 11:54:42 -0400

    Update pipeline-config commit again.

--- src/zero/boot.clj
+++ src/zero/boot.clj
@@ -114,7 +114,7 @@
     (let [pipeline-config (clone "pipeline-config" "wfl/environments.clj")]
       (stage (clone "dsde-pipelines" "tasks/CopyFilesFromCloudToCloud.wdl"))
       (util/shell-io! "git" "-C" (.getParent pipeline-config)
-                      "checkout" "8d2db1b83be73e2309e560cff6d31d3c7e3e7c32")
+                      "checkout" "410da0689470c4a916b36c8f5af38e3e852b860a")
       (stage pipeline-config))))
 
 (defn adapterize-wgs
