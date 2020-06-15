c1118c6982724f3fadc1e77018b3601923c77eb0
sahakiann <sahakian@broadinstitute.org>
2020-06-15 17:22:58 -0400

    updated dsde-pipelines branch

--- src/zero/module/wgs.clj
+++ src/zero/module/wgs.clj
@@ -48,7 +48,7 @@
 
 (def workflow-wdl
   "The top-level WDL file and its version."
-  {:release "tl_external_wgs_reprocessing_PO-26024"
+  {:release "tl_PO-26024_custom_gatk"
    :top     "pipelines/reprocessing/external/wgs/ExternalWholeGenomeReprocessing.wdl"})
 
 (def cromwell-label-map
