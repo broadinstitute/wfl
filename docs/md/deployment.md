ca503a3bf0532fa8f8e9d4b45c65582bd4314a50
Tom Lyons <tbl@broadinstitute.org>
2020-06-22 15:33:01 -0400

    GH-803: Fix typos. (#53)
    * Add build instructions.
    * Show what cli.py is running.
    * Show version in JSON too.
    * Thread WFL_IMAGE through the workflow.
    * Fail when a subprocess fails.
    * FML: render-ctmpls always fails.
    * Pretend Python is typed.
    * Warn that render-ctmpls fails.
    * Update ops/cli.py as you wish.
    Co-authored-by: Edmund Higham <ehigham@users.noreply.github.com>
    * Update ops/cli.py
    Co-authored-by: Chengchen(Rex) Wang <14366016+rexwangcc@users.noreply.github.com>
    * *BUMP*
    Co-authored-by: Edmund Higham <ehigham@users.noreply.github.com>
    Co-authored-by: Chengchen(Rex) Wang <14366016+rexwangcc@users.noreply.github.com>

--- docs/md/deployment.md
+++ docs/md/deployment.md
@@ -1,78 +1,185 @@
 # Deployment of WorkFlow Launcher
 
 ## Make a deployment
-The WorkFlow Launcher is currently running on a Kubernetes cluster, and the deployment is managed by [Helm](https://helm.sh/docs/intro/install/). In order to make a deployment or upgrade the current deployment manually, you have to make the following preparations:
+
+The WorkFlow Launcher is currently running on a Kubernetes cluster,
+and the deployment is managed by [Helm](https://helm.sh/docs/intro/install/).
+In order to make a deployment or upgrade the current deployment manually,
+you have to make the following preparations:
 
 ### Setup Kubernetes
-- Make sure you have the `kubectl` command available, while in the Broad network or using the VPN, run a command like the following to setup the connection to the desired cluster:
-    ```bash
-    gcloud container clusters get-credentials gotc-dev-shared-us-central1-a --zone us-central1-a --project broad-gotc-dev
-    ```
-- Run `kubectl config get-contexts` to make sure you are connected to the right cluster.
-- Later you could run `kubectl config use-context $CONTEXT_NAME` to switch (back) to other contexts.
+
+- Make sure you have the `kubectl` command available,
+  while in the Broad network or using the VPN,
+  run a command like the following
+  to set up the connection to the desired cluster:
+
+  ```bash
+  gcloud container clusters get-credentials \
+    gotc-dev-shared-us-central1-a --zone us-central1-a \
+    --project broad-gotc-dev
+  ```
+
+- Run `kubectl config get-contexts` to make sure you are connected
+  to the right cluster.
+
+- Later you could run `kubectl config use-context $CONTEXT_NAME`
+  to switch (back) to other contexts.
 
 ### Setup Helm
-- Install Helm, please follow the Helm instructions or simply try `brew install helm`, assuming you have [HomeBrew](https://brew.sh/) installed on your macOS.
-- Run:
-    ```bash
-    helm repo add gotc-charts https://broadinstitute.github.io/gotc-helm-repo/
-    ```
-    to add gotc’s Helm repo to your Helm. Note `gotc-charts` is just an alias, you could give it any name you want.
+
+- Install Helm,
+  please follow the Helm instructions
+  or simply try `brew install helm`,
+  assuming you have [HomeBrew](https://brew.sh/)
+  installed on your MacOS.
 
 - Run:
-    ```bash
-    helm repo update
-    ```
-    to make the local cached charts update-to-date to the remote repo and also run:
 
-    ```bash
-    helm repo list
-    ```
-    to check the list of repo you have connected to anytime you want.
+  ```bash
+  helm repo add gotc-charts https://broadinstitute.github.io/gotc-helm-repo/
+  ```
+  to add gotc’s Helm repo to your Helm.
+  Note `gotc-charts` is just an alias, you could give it any name you want.
 
-- In the Broad network or on VPN and your `kubectl` is setup to connect to the right cluster,  run:
-    ```bash
-    helm list
-    ```
-    to check the current deployments that are managed by Helm.
+- Run:
 
-### Deployment
-Once you have finished the above preparations, you could take a look at your custom values for the deployment. Usually that means a rendered version of [these YML files](https://github.com/broadinstitute/gotc-deploy/tree/master/deploy/gotc-dev/helm), with your modifications to some specific values in it.
+  ```bash
+  helm repo update
+  ```
+  to make the local cached charts update-to-date
+  to the remote repo and also run:
 
-**Note:** some of the values in these YML files contain credentials or sensitive information, so **DO NOT** checkout them into your version control system or make them public!!!
+  ```bash
+  helm repo list
+  ```
+  to check the list of repo you have connected to anytime you want.
 
-Now with Helm and the custom values YML files, you could run for instance:
+- In the Broad network or on VPN and your `kubectl`
+  is setup to connect to the right cluster,  run:
+
+  ```bash
+  helm list
+  ```
+  to check the current deployments that are managed by Helm.
+
+### Build
+
+Build a new WFL jar it you want one.
 
 ```bash
-helm install gotc-dev gotc-charts/authproxy -f custom-authvals.yaml
+boot build
 ```
-where:
 
-- `gotc-dev` is the name of the deployment
-- `gotc-charts/authproxy` is following `chart-repo-alias/chartname`
-- `custom-authvals.yaml` is the path to your custom values YML file
+### Dockerize
 
-to make a new deployment or
+Compose a new docker image if you want one.
+Tag it with some helpful name,
+then push it to DockerHub
+so Kubernetes can find it.
+
 ```bash
-helm upgrade gotc-dev gotc-charts/authproxy -f custom-authvals.yaml
+docker build -t broadinstitute/workflow-launcher-api .
+docker push broadinstitute/workflow-launcher-api:latest
 ```
-To update an existing deployment without re-creating all of the resources which could slow down the process.
 
-You could run `kubectl get pods` to check the readiness of your deployments, `kubectl logs $POD_NAME $POD_APPLICATION_NAME` to view the logs.
+You should see it listed here.
+
+https://hub.docker.com/repository/docker/broadinstitute/workflow-launcher-api
+
+### Clone the gotc-deploy repository.
+
+``` bash
+git clone --branch <the branch you want> \
+  https://github.com/broadinstitute/gotc-deploy.git
+```
+
+### Render the chart
+
+Render the `wfl-values.yaml` file.
+
+``` bash
+docker run -i --rm -v "$(pwd)":/working \
+  -v "$HOME"/.vault-token:/root/.vault-token \
+  -e WFL_VERSION=latest \
+  broadinstitute/dsde-toolbox:dev \
+  /usr/local/bin/render-ctmpls.sh \
+  -k ./gotc-deploy/deploy/gotc-dev/helm/wfl-values.yaml.ctmpl
+```
+
+**Note:**
+That command always fails,
+so look at `./gotc-deploy/deploy/gotc-dev/helm/wfl-values.yaml`
+to verify that the values
+substituted into the template are correct.
+
+**Note:**
+Some of the values in these YML files
+contain credentials or sensitive information,
+so **DO NOT** check them in
+to your version control system
+or make them public!!!
+
+### Deploy
+
+Run this to upgrade a deployment release.
+
+```bash
+helm upgrade wfl-k8s gotc-charts/wfl --install \
+  -f ./gotc-deploy/deploy/gotc-dev/helm/wfl-values.yaml
+```
+
+That doesn't recreate ingress resources and so on,
+but it will `--install` everything necessary
+if a release is not deployed already.
+
+Run `kubectl get pods` to review your deployments.
+
+``` bash
+# kubectl get pods
+NAME                                              READY STATUS  AGE
+cromwell-auth-gotc-dev-authproxy-6f76598bb4-snpp6 1/1   Running 11d
+hjf-test-authproxy-78758bbb6b-7lnfg               2/2   Running 42d
+wfl-k8s-6948f6566d-2bdcp                          2/2   Running 2d5h
+#
+```
+
+Run `kubectl logs wfl-k8s-6948f6566d-2bdcp workflow-launcher-api`
+to view the logs for the WFL-API server.
+
+Run `kubectl logs wfl-k8s-6948f6566d-2bdcp workflow-launcher-ui`
+to view the logs for the WFL-UI front end.
+
+[//]: # (tbl stopped fixing stuff here.)
 
 ## Testing the deployment locally
-Similar to the above process, but in addition to the above preparations, you also have to:
 
-- Make sure you have a relatively new version of Docker client installed, which has Docker -> Preferences -> Kubernetes -> Enable Kubernetes. Turn on that options and restart your Docker client.
-- Run `kubectl config use-context $CONTEXT_NAME` to the Docker-built-in Kubernetes context, usually it should be called something like `docker-for-mac`.
+Similar to the above process,
+but in addition to the above preparations,
+you also have to:
+
+- Make sure you have a relatively new version of Docker client installed,
+  which has Docker -> Preferences -> Kubernetes -> Enable Kubernetes.
+  Turn on that options and restart your Docker client.
+
+- Run `kubectl config use-context $CONTEXT_NAME`
+  to the Docker-built-in Kubernetes context,
+  usually it should be called something like `docker-for-mac`.
 
 Similar to how you have setup the Helm charts, you could run:
 
 ```bash
 helm install gotc-dev gotc-charts/authproxy -f custom-authvals.yaml
 ```
+
 Or
+
 ```bash
 helm upgrade gotc-dev gotc-charts/authproxy -f custom-authvals.yaml
 ```
-To install or upgrade the deployments on your local cluster. One thing to note is that you **CANNOT** create an `ingress` resource locally, so it’s important to `disable` the ingress creation in your custom values YML files.
+
+To install or upgrade the deployments on your local cluster.
+One thing to note is that you **CANNOT** create
+an `ingress` resource locally,
+so it’s important to `disable` the ingress creation
+in your custom values YML files.
--- ops/cli.py
+++ ops/cli.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
-"""WFL Deployment Script 
+"""WFL Deployment Script
 @rex
 
 requirements: pip3 install gitpython
 
+Example: ./ops/cli.py -t latest
+
 usage: python3 cli.py -h
 """
-import argparse
-import sys
-from typing import Callable
-import subprocess
-from pathlib import Path
-import tempfile
-import uuid
-import git
-import shutil
 from enum import Enum
+from pathlib import Path
+from typing import Callable
+import argparse
+import git
 import os
+import shutil
+import subprocess
 import sys
-import logging
-
+import tempfile
 
 # In case this will need to run on Windows systems
 if sys.platform.lower() == "win32":
@@ -68,11 +66,10 @@ def register(func: Callable) -> Callable:
 class CLI:
     def __init__(self):
         parser = argparse.ArgumentParser(
-            description="WFL Development CLI", usage=self._usage()
+            description="Deploy the Workflow Launcher API and UI.",
+            usage=self._usage()
         )
-        parser.add_argument("command", help="command from the above list to run")
-
-        # Print help if no command provided
+        parser.add_argument("command", help="command from above to run")
         if len(sys.argv[1:2]) == 0:
             parser.print_help()
             exit(1)
@@ -86,35 +83,53 @@ class CLI:
         return getattr(self, args.command)(sys.argv[2:])
 
     def _usage(self) -> str:
-        msg, space = "\n", 20
+        max = 0
         for command in sorted(self.registered_commands):
-            msg += f"   {command}{'  ' * (space - len(command))} |-> {getattr(self, command).__doc__}\n"
+            if len(command) > max:
+                max = len(command)
+        msg = "\n"
+        for command in sorted(self.registered_commands):
+            msg += f"  {command}{' ' * (max - len(command))}"
+            msg += f" |-> {getattr(self, command).__doc__}\n"
         return msg
+
+    @staticmethod
+    def _subprocess_call(command: str):
+        """Run COMMAND in a subprocess."""
+        print(f"Running: {command}")
+        return subprocess.check_call(command, shell=True)
+
+    @staticmethod
+    def _subprocess_call_unchecked(command: str):
+        """Run COMMAND in a subprocess and who cares whether it fails!"""
+        print(f"Running: {command}")
+        return subprocess.call(command, shell=True)
 
     @register
     def render(self, arguments: list) -> int:
-        """Render a CTMPL file"""
-        parser = argparse.ArgumentParser(description=f"%(prog) {self.render.__doc__}")
-        parser.add_argument("file", help="Path to the CTMPL file needs to be rendered")
+        """Render a CTMPL file."""
+        parser = argparse.ArgumentParser(
+            description=f"%(prog) {self.render.__doc__}"
+        )
+        parser.add_argument("file", help="CTMPL file to be rendered")
         args = parser.parse_args(arguments)
-
         file = Path(args.file)
         assert file.exists() and file.is_file(), f"{file} is not valid!"
         return CLI._render_ctmpl(ctmpl_file=str(file))
 
     @staticmethod
     def _clone_config_repo(dest: str, branch: str = "master"):
-        """Clone the gotc-deploy repo, this requires permission to clone private Broad repos."""
+        """Clone the gotc-deploy repo."""
         print(
             dye_msg_with_color(
-                msg=f"=> Cloning gotc-deploy repo to {dest}, default branch: {branch}",
-                color="blue",
+                msg=f"=> Cloning gotc-deploy repo to {dest}, branch: {branch}",
+                color="blue"
             )
         )
         git.Repo.clone_from(
             url="git@github.com:broadinstitute/gotc-deploy.git",
             to_path=dest,
-            branch=branch,
+            branch=branch
         )
         print(
             dye_msg_with_color(
@@ -134,17 +149,25 @@ class CLI:
         envs = ""
         if kwargs:
             for k, v in kwargs.items():
-                envs += f"-e {k}={v} "
-            print(dye_msg_with_color(msg=f"=> Feeding variables: {envs}", color="blue"))
-            COMMAND = f'docker run -i --rm -v "$(pwd)":/working -v "$HOME"/.vault-token:/root/.vault-token {envs} broadinstitute/dsde-toolbox:dev /usr/local/bin/render-ctmpls.sh -k "{ctmpl_file}"'
-        else:
-            COMMAND = f'docker run -i --rm -v "$(pwd)":/working -v "$HOME"/.vault-token:/root/.vault-token broadinstitute/dsde-toolbox:dev /usr/local/bin/render-ctmpls.sh -k "{ctmpl_file}"'
+                envs += f"-e {k}={v}"
+            print(
+                dye_msg_with_color(
+                    msg=f"=> Feeding variables: {envs}", color="blue"
+                )
+            )
+        command = " ".join(['docker run -i --rm -v "$(pwd)":/working',
+                            '-v "$HOME"/.vault-token:/root/.vault-token',
+                            f'{envs}',
+                            'broadinstitute/dsde-toolbox:dev',
+                            '/usr/local/bin/render-ctmpls.sh -k',
+                            f'"{ctmpl_file}"'])
         print(
             dye_msg_with_color(
-                msg=f"[✔] Rendered file {ctmpl_file.split('.ctmpl')[0]}", color="green"
+                msg=f"[✔] Rendered file {ctmpl_file.split('.ctmpl')[0]}",
+                color="green"
             )
         )
-        return subprocess.call(COMMAND, shell=True)
+        CLI._subprocess_call_unchecked(command)
 
     @staticmethod
     def _is_available(*commands: list):
@@ -152,120 +175,127 @@ class CLI:
             if not shutil.which(cmd):
                 print(
                     dye_msg_with_color(
-                        f"=> {cmd} is missing in PATH, please check and install!",
-                        color="red",
+                        f"=> {cmd} is not in PATH. Please install it!",
+                        color="red"
                     )
                 )
 
     @staticmethod
     def _set_up_helm() -> int:
-        print(dye_msg_with_color(msg=f"=> Setting up Helm charts", color="blue"))
-        HELM_ADD = f"helm repo add gotc-charts https://broadinstitute.github.io/gotc-helm-repo/;"
-        HELM_REPO = f"helm repo update;"
-        print(dye_msg_with_color(msg=f"[✔] Set up Helm charts", color="green"))
-        return subprocess.call(HELM_ADD + HELM_REPO, shell=True)
+        print(dye_msg_with_color(msg="=> Setting up Helm charts", color="blue"))
+        ADD = " ".join(["helm repo add gotc-charts",
+                        "https://broadinstitute.github.io/gotc-helm-repo/"])
+        REPO = "helm repo update"
+        print(dye_msg_with_color(msg="[✔] Set up Helm charts", color="green"))
+        CLI._subprocess_call(ADD)
+        CLI._subprocess_call(REPO)
 
     @staticmethod
     def _set_up_k8s(environment: str = "dev", namespace: str = "") -> int:
-        """Connect to K8S cluster and setup namespace with gcloud and kubectl."""
+        """Connect to K8S cluster and set up namespace."""
+        nsorna = namespace or 'N/A'
         print(
             dye_msg_with_color(
-                msg=f"=> Setting up Kubernetes, with namespace {namespace or 'N/A' }",
+                msg=f"=> Setting up Kubernetes with namespace {nsorna}.",
                 color="blue",
             )
         )
-        GCLOUD_SETUP = f"gcloud container clusters get-credentials gotc-{environment}-shared-us-central1-a --zone us-central1-a --project broad-gotc-{environment}"
-        code1 = subprocess.call(GCLOUD_SETUP, shell=True)
-
+        ZONE = "us-central1-a"
+        GCLOUD = " ".join(["gcloud container clusters get-credentials",
+                           f"gotc-{environment}-shared-{ZONE} --zone {ZONE}",
+                           f"--project broad-gotc-{environment}"])
+        CLI._subprocess_call(GCLOUD)
         print(
             dye_msg_with_color(
-                msg=f"=> Setting up Kubernetes cluster context", color="blue"
+                msg="=> Setting up Kubernetes cluster context.", color="blue"
             )
         )
-        CTX = f"gke_broad-gotc-{environment}_us-central1-a_gotc-{environment}-shared-us-central1-a"
-        KUBE_CONTEXT = f"kubectl config use-context {CTX}"
-        code2 = subprocess.call(KUBE_CONTEXT, shell=True)
-
-        code3 = 0
+        CONTEXT = "_".join([f"gke_broad-gotc-{environment}_{ZONE}",
+                            f"gotc-{environment}-shared-{ZONE}"])
+        CLI._subprocess_call(f"kubectl config use-context {CONTEXT}")
         if namespace:
-            KUBE_SET_NAMESPACE = f"kubectl config set-context $(kubectl config current-context) --namespace={namespace}"
-            code3 = subprocess.call(KUBE_SET_NAMESPACE, shell=True)
+            CLI._subprocess_call(
+                f"kubectl config set-context {CONTEXT} --namespace={namespace}"
+            )
             print(
                 dye_msg_with_color(
-                    msg=f"[✔] Set up namespace to {namespace}", color="green"
+                    msg=f"[✔] Set namespace to {namespace}.", color="green"
                 )
             )
-        return any([code1, code2, code3])
 
     @staticmethod
     def _helm_deploy_wfl(values: str) -> int:
-        """Use Helm to deploy WFL to K8S."""
+        """Use Helm to deploy WFL to K8S using VALUES."""
         print(
             dye_msg_with_color(
-                msg=f"=> Deploying to K8S cluster with Helm (this requires VPN)",
-                color="blue",
+                msg="=> Deploying to K8S cluster with Helm.",
+                color="blue"
             )
         )
-        HELM_UPGRADE = f"helm upgrade wfl-k8s gotc-charts/wfl -f {values} --install"
         print(
             dye_msg_with_color(
-                msg=f"[✔] WFL is deployed, run `kubectl get pods` to see details",
-                color="green",
+                msg="=> This must run on a non-split VPN.",
+                color="blue"
             )
         )
-        return subprocess.call(HELM_UPGRADE, shell=True)
+        UPGRADE = f"helm upgrade wfl-k8s gotc-charts/wfl -f {values} --install"
+        CLI._subprocess_call(UPGRADE)
+        print(
+            dye_msg_with_color(
+                msg="[✔] WFL is deployed. Run `kubectl get pods` for details.",
+                color="green"
+            )
+        )
 
     @register
     def deploy(self, arguments):
         """Deploy WFL to Cloud GKE (VPN is required)"""
         parser = argparse.ArgumentParser(description=f"{self.deploy.__doc__}")
         parser.add_argument(
+            "-b",
+            "--branch",
+            dest="branch",
+            default="master",
+            help="Use this branch of gotc-deploy."
+        )
+        parser.add_argument(
             "-e",
             "--environment",
             dest="environment",
             default="dev",
-            choices={"dev",},
-            help="Environment to deploy to",
-        )
-        parser.add_argument(
-            "-v",
-            "--version",
-            dest="version",
-            default="latest",
-            help="Which version of WFL to deploy, i.e. a WFL version string or [latest] by default",
+            choices={"dev"},
+            help="Deploy to this environment."
         )
         parser.add_argument(
             "-n",
             "--namespace",
             dest="namespace",
             default="",
-            help="The namespace you want to use for deployemnt",
+            help="Deploy to this namespace."
+        )
+        parser.add_argument(
+            "-t",
+            "--tag",
+            dest="tag",
+            default="latest",
+            help="Deploy image with this tag in DockerHub."
         )
         args = parser.parse_args(arguments)
-
         CLI._is_available("helm", "kubectl", "gcloud", "git", "docker")
         CLI._set_up_helm()
         CLI._set_up_k8s(namespace=args.namespace)
-
-        with tempfile.TemporaryDirectory(
-            prefix=str(uuid.uuid4()), dir=Path("./")
-        ) as dir_name:
-            CLI._clone_config_repo(
-                dest=Path(f"./{dir_name}/gotc-deploy"), branch="master"
-            )
-
-            file_name = "wfl-values.yaml.ctmpl"
-            shutil.copy(
-                str(Path(f"./{dir_name}/gotc-deploy/deploy/gotc-dev/helm/{file_name}")),
-                Path(f"./{dir_name}/{file_name}"),
-            )
+        with tempfile.TemporaryDirectory() as dir:
+            pwd = os.getcwd()
+            os.chdir(dir)
+            CLI._clone_config_repo(dest="gotc-deploy", branch=args.branch)
+            values = "wfl-values.yaml"
+            shutil.copy(f"gotc-deploy/deploy/gotc-dev/helm/{values}.ctmpl", dir)
             CLI._render_ctmpl(
-                ctmpl_file=str(Path(f"./{dir_name}/{file_name}")),
-                WFL_VERSION=args.version,
+                ctmpl_file=f"{values}.ctmpl", WFL_VERSION=args.tag
             )
-            CLI._helm_deploy_wfl(values=str(Path(f"./{dir_name}/wfl-values.yaml")))
-
-        print(dye_msg_with_color(msg=f"[✔] Deployment is done!", color="green",))
+            CLI._helm_deploy_wfl(values=values)
+            os.chdir(pwd)
+        print(dye_msg_with_color(msg="[✔] Deployment is done!", color="green"))
 
 
 if __name__ == "__main__":
