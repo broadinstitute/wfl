779d98033d00e369b8abcedbaada60ffdab11f81
Tom Lyons <tbl@broadinstitute.org>
2020-06-30 11:35:30 -0400

    GH-806: Debug WFL connection refused exception. (#57)
    * Tell Jetty to STFU.
    * Simplify.
    * Make Flake8 happy. :sigh:
    * May as well build and dockerize too.
    * Back to the silly :debug environment.

--- src/zero/once.clj
+++ src/zero/once.clj
@@ -8,54 +8,47 @@
             [zero.zero         :as zero])
   (:import [com.google.auth.oauth2 GoogleCredentials ServiceAccountCredentials]))
 
-(defn- authorization-header-with-bearer-token
+(defn authorization-header-with-bearer-token
   "An Authorization header with a Bearer TOKEN."
   [token]
   {"Authorization" (str/join \space ["Bearer" token])})
 
-(defn- get-service-account-from-environment
-  "Throw or get the service account source from ZERO_DEPLOY_ENVIRONMENT."
+(defn service-account-credentials
+  "Throw or return credentials for the WFL service account."
   []
-  (let [environment (util/getenv "ZERO_DEPLOY_ENVIRONMENT" "debug")
-        env (zero/throw-or-environment-keyword! environment)]
-    (get-in env/stuff [env :server :service-account])))
+  (some->
+    (if-let [file (util/getenv "GOOGLE_APPLICATION_CREDENTIALS")]
+      (-> file io/file)
+      (-> "ZERO_DEPLOY_ENVIRONMENT"
+        (util/getenv "debug")
+        zero/error-or-environment-keyword
+        env/stuff :server :service-account util/vault-secrets
+        (json/write-str :escape-slash false)
+        .getBytes))
+    io/input-stream GoogleCredentials/fromStream
+    (.createScoped ["https://www.googleapis.com/auth/cloud-platform"
+                    "https://www.googleapis.com/auth/userinfo.email"
+                    "https://www.googleapis.com/auth/userinfo.profile"])))
 
-(defn- service-account-credentials
-  "Throw or return credentials for the service account SA."
-  [{:keys [file vault] :as sa}]
-  (letfn [(jsonify [edn] (json/write-str edn :escape-slash false))]
-    (-> (cond file  (io/file file)
-              vault (-> vault util/vault-secrets jsonify .getBytes)
-              :else (throw (IllegalArgumentException. (pr-str sa))))
-        io/input-stream GoogleCredentials/fromStream
-        (.createScoped ["https://www.googleapis.com/auth/cloud-platform"
-                        "https://www.googleapis.com/auth/userinfo.email"
-                        "https://www.googleapis.com/auth/userinfo.profile"]))))
+(defn service-account-email
+  "The client_email for the WFL service account."
+  []
+  (.getClientEmail (service-account-credentials)))
 
-(defn- service-account-token
-  "Throw or return a bearer token for the service account SA."
-  [sa]
-  (-> sa service-account-credentials .refreshAccessToken .getTokenValue))
+(defn service-account-token
+  "A bearer token for the WFL service account."
+  []
+  (-> (service-account-credentials) .refreshAccessToken .getTokenValue))
+
+(defn get-service-account-header
+  "An Authorization header with a service account Bearer token."
+  []
+  (authorization-header-with-bearer-token (service-account-token)))
 
 (defn get-auth-header
   "An Authorization header with a Bearer token."
   []
   (authorization-header-with-bearer-token
-   (if-let [environment (util/getenv "ZERO_DEPLOY_ENVIRONMENT")]
-     (let [env (zero/throw-or-environment-keyword! environment)
-           sa (get-in env/stuff [env :server :service-account])]
-       (service-account-token sa))
-     (util/shell! "gcloud" "auth" "print-access-token"))))
-
-(defn get-service-account-header
-  "An Authorization header with service account Bearer token."
-  []
-  (authorization-header-with-bearer-token
-   (service-account-token
-    (get-service-account-from-environment))))
-
-(defn service-account-email
-  "The client_email for the ZERO_DEPLOY_ENVIRONMENT service account."
-  []
-  (-> (get-service-account-from-environment)
-      service-account-credentials .getClientEmail))
+    (if (util/getenv "ZERO_DEPLOY_ENVIRONMENT")
+      (service-account-token)
+      (util/shell! "gcloud" "auth" "print-access-token"))))
