3ac0111730abb9d582c2fef025e6f2571ae5799b
Edmund Higham <edhigham@gmail.com>
2020-07-28 16:46:20 +0100

    include copyfile.wdl in jar. Extract to a temporary file before sending

--- src/zero/module/copyfile.clj
+++ src/zero/module/copyfile.clj
@@ -5,7 +5,8 @@
             [zero.module.all :as all]
             [zero.service.cromwell :as cromwell]
             [zero.service.postgres :as postgres]
-            [zero.util :as util])
+            [zero.util :as util]
+            [zero.wdl :as wdl])
   (:import [java.time OffsetDateTime]))
 
 (def pipeline "copyfile")
@@ -18,12 +19,12 @@
   "Submit WORKFLOW to Cromwell in ENVIRONMENT."
   [environment workflow]
   (cromwell/submit-workflow
-   environment
-   (io/file (:top workflow-wdl))
-   nil
-   (-> workflow (select-keys [:src :dst]) (util/prefix-keys pipeline))
-   (util/make-options environment)
-   {}))
+    environment
+    (util/extract-resource (:top workflow-wdl))
+    nil
+    (-> workflow (select-keys [:src :dst]) (util/prefix-keys pipeline))
+    (util/make-options environment)
+    {}))
 
 (defn add-workload!
   "Use transaction TX to add the workload described by BODY."
@@ -44,8 +45,8 @@
             (update! [tx [id uuid]]
               (when uuid
                 (jdbc/update! tx items
-                              {:updated now :uuid uuid}
-                              ["id = ?" id])))]
-        (let [workflow (postgres/get-table tx items)]
-          (jdbc/update! tx :workload {:started now} ["uuid = ?" uuid])
-          (run! (comp (partial update! tx) submit!) workflow)))))
+                  {:updated now :uuid uuid}
+                  ["id = ?" id])))]
+      (let [workflow (postgres/get-table tx items)]
+        (jdbc/update! tx :workload {:started now} ["uuid = ?" uuid])
+        (run! (comp (partial update! tx) submit!) workflow)))))
