8fab29502677c568cbcb5416d7ba4b2250251ca3
Jack Warren <jack@jackwarren.info>
2020-07-31 10:15:46 -0400

    [GH-764] Add better endpoint logging (#77)
    * Bug fixes to memoization
    
    * Add endpoint logging
    
    * Fix the nil return issue with db tx macro
    
    * Update src/zero/api/handlers.clj with Rex's fix
    
    Co-authored-by: Chengchen(Rex) Wang <14366016+rexwangcc@users.noreply.github.com>
    
    * More narrowly log handler parameters
    
    * More narrowly log handler parameters (nit)
    
    * Make DB queries trace so they don't show up
    
    * Add a macro to print errors that rise through handlers
    
    Co-authored-by: Rex Wang <rexwangcc@gmail.com>
    Co-authored-by: Chengchen(Rex) Wang <14366016+rexwangcc@users.noreply.github.com>

--- src/zero/jdbc.clj
+++ src/zero/jdbc.clj
@@ -3,20 +3,30 @@
   (:require [clojure.java.jdbc :as jdbc]
             [clojure.tools.logging.readable :as log]))
 
-(defn clean-db
-  "Remove password from DB."
+(def ^:private remember-db
+  "Memoized helper to only print the db out the first time"
+  (memoize (fn [db]
+             (let [random (rand-int 10000)
+                   identifier (str (:user db) "@" (:connection-uri db) "#" random)]
+               (log/infof "JDBC new DB config %s observed (the #%s isn't important, just unique):"
+                          identifier random)
+               (log/info (dissoc db :password))
+               identifier))))
+
+(defn format-db
+  "Return the db config's identifier, printing the whole db if it is new."
   [db]
-  (dissoc db :password))
+  (zero.jdbc/remember-db (into {} (filter #(-> % last string?) db))))
 
 (defmacro query
   "Logged alias for [[clojure.java.jdbc/query]]"
   ([db sql-params]
    `(do
-      (log/info "JDBC SQL query (without opts):" (clean-db ~db) ~sql-params)
+      (log/trace "JDBC SQL query (without opts):" (format-db ~db) ~sql-params)
       (jdbc/query ~db ~sql-params)))
   ([db sql-params opts]
    `(do
-      (log/info "JDBC SQL query:" (clean-db ~db) ~sql-params ~opts)
+      (log/trace "JDBC SQL query:" (format-db ~db) ~sql-params ~opts)
       (jdbc/query ~db ~sql-params ~opts))))
 
 (defmacro update!
@@ -24,12 +34,12 @@
   ([db table set-map where-clause]
    `(do
       (log/info "JDBC SQL update! (without opts):"
-        (clean-db ~db) ~table ~set-map ~where-clause)
+        (format-db ~db) ~table ~set-map ~where-clause)
       (jdbc/update! ~db ~table ~set-map ~where-clause)))
   ([db table set-map where-clause opts]
    `(do
       (log/info "JDBC SQL update!:"
-        (clean-db ~db) ~table ~set-map ~where-clause ~opts)
+        (format-db ~db) ~table ~set-map ~where-clause ~opts)
       (jdbc/update! ~db ~table ~set-map ~where-clause ~opts))))
 
 (defmacro insert-multi!
@@ -37,43 +47,43 @@
   ([db table rows]
    `(do
       (log/info "JDBC SQL insert-rows! (without opts):"
-        (clean-db ~db) ~table ~rows)
+        (format-db ~db) ~table ~rows)
       (jdbc/insert-multi! ~db ~table ~rows)))
   ([db table cols-or-rows values-or-opts]
    `(do
       (if (map? values-or-opts)
         (log/info "JDBC SQL insert-rows!:"
-          (clean-db ~db) ~table ~cols-or-rows ~values-or-opts)
+          (format-db ~db) ~table ~cols-or-rows ~values-or-opts)
         (log/info "JDBC SQL insert-cols! (without opts):"
-          (clean-db ~db) ~table ~cols-or-rows ~values-or-opts))
+          (format-db ~db) ~table ~cols-or-rows ~values-or-opts))
       (jdbc/insert-multi! ~db ~table ~cols-or-rows ~values-or-opts)))
   ([db table cols values opts]
    `(do
       (log/info "JDBC SQL insert-cols!:"
-        (clean-db ~db) ~table ~cols ~values ~opts)
+        (format-db ~db) ~table ~cols ~values ~opts)
       (jdbc/insert-multi! ~db ~table ~cols ~values ~opts))))
 
 (defmacro execute!
   "Logged alias for [[clojure.java.jdbc/execute!]]"
   ([db sql-params]
    `(do
-      (log/info "JDBC SQL execute! (without opts):" (clean-db ~db) ~sql-params)
+      (log/info "JDBC SQL execute! (without opts):" (format-db ~db) ~sql-params)
       (jdbc/execute! ~db ~sql-params)))
   ([db sql-params opts]
    `(do
-      (log/info "JDBC SQL execute!:" (clean-db ~db) ~sql-params ~opts)
+      (log/info "JDBC SQL execute!:" (format-db ~db) ~sql-params ~opts)
       (jdbc/execute! ~db ~sql-params ~opts))))
 
 (defmacro db-do-commands
   "Logged alias for [[clojure.java.jdbc/db-do-commands]]"
   ([db sql-commands]
    `(do
-      (log/info "JDBC SQL db-do-commands:" (clean-db ~db) ~sql-commands)
+      (log/info "JDBC SQL db-do-commands:" (format-db ~db) ~sql-commands)
       (jdbc/db-do-commands ~db ~sql-commands)))
   ([db transaction? sql-commands]
    `(do
       (log/info "JDBC SQL db-do-commands:"
-        (clean-db ~db) ~transaction? ~sql-commands)
+        (format-db ~db) ~transaction? ~sql-commands)
       (jdbc/db-do-commands ~db ~transaction? ~sql-commands))))
 
 (defmacro insert!
@@ -81,20 +91,20 @@
   ([db table row]
    `(do
       (log/info "JDBC SQL insert-rows! (without opts):"
-        (clean-db ~db) ~table [~row])
+        (format-db ~db) ~table [~row])
       (jdbc/insert! ~db ~table ~row)))
   ([db table cols-or-row values-or-opts]
    `(do
       (if (map? values-or-opts)
         (log/info "JDBC SQL insert-rows!:"
-          (clean-db ~db) ~table [~cols-or-row] ~values-or-opts)
+          (format-db ~db) ~table [~cols-or-row] ~values-or-opts)
         (log/info "JDBC SQL insert-cols! (without opts):"
-          (clean-db ~db) ~table ~cols-or-row [~values-or-opts]))
+          (format-db ~db) ~table ~cols-or-row [~values-or-opts]))
       (jdbc/insert! ~db ~table ~cols-or-row ~values-or-opts)))
   ([db table cols values opts]
    `(do
       (log/info "JDBC SQL insert-cols!:"
-        (clean-db ~db) ~table ~cols [~values] ~opts)
+        (format-db ~db) ~table ~cols [~values] ~opts)
       (jdbc/insert! ~db ~table ~cols ~values ~opts))))
 
 (defmacro with-db-transaction
@@ -102,7 +112,7 @@
   [binding & body]
   `(let [id# (rand-int 10000)]
      (log/info "JDBC SQL transaction" id# "started to"
-       (clean-db ~(second binding)))
+       (format-db ~(second binding)))
      (let [exe# (jdbc/with-db-transaction ~binding ~@body)]
        (log/info "JDBC SQL transaction" id# "ended")
        exe#)))
@@ -119,9 +129,9 @@
   "Logged alias for [[clojure.java.jdbc/prepare-statement]]"
   ([db-spec]
    `(do
-      (log/info "JBDC SQL connection made (no opts):" (clean-db ~db-spec))
+      (log/info "JBDC SQL connection made (no opts):" (format-db ~db-spec))
       (jdbc/get-connection ~db-spec)))
   ([db-spec opts]
    `(do
-      (log/info "JBDC SQL connection made:" (clean-db ~db-spec) ~opts)
+      (log/info "JBDC SQL connection made:" (format-db ~db-spec) ~opts)
       (jdbc/get-connection ~db-spec ~opts))))
