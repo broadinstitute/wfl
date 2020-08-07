49d0fec71ca935ef8ce02dad49efa62d264e5f99
Edmund Higham <edhigham@gmail.com>
2020-08-07 21:02:59 +0100

    fix build failure in github. cause: using sh instead of bash

--- makerules/Python.mk
+++ makerules/Python.mk
@@ -13,22 +13,22 @@ VIRTUAL_ENVIRONMENT_DIR    := $(DERIVED_DIR)/.venv
 PYTHON_VIRTUAL_ENVIRONMENT := \
 	$(addsuffix /$(MODULE),$(VIRTUAL_ENVIRONMENT_DIR))
 
-DISTCLEAN_FILES += $(PYTHON_VIRTUAL_ENVIRONMENT).$(TS)
-DISTCLEAN_DIRS  += $(PYTHON_VIRTUAL_ENVIRONMENT)
+# Usage: $(call using-python-enviroment, command[s])
+define using-python-environment
+	$(SOURCE) $(PYTHON_VIRTUAL_ENVIRONMENT)/bin/activate && ( $1 )
+endef
 
 $(PREBUILD): $(PYTHON_VIRTUAL_ENVIRONMENT).$(TS)
 $(PYTHON_VIRTUAL_ENVIRONMENT).$(TS): | $(VIRTUAL_ENVIRONMENT_DIR)
 $(PYTHON_VIRTUAL_ENVIRONMENT).$(TS): $(REQUIRED_PYTHON_ENVIRONMENT)
-	$(PYTHON) -m venv $(PYTHON_VIRTUAL_ENVIRONMENT);      \
-	$(SOURCE) $(PYTHON_VIRTUAL_ENVIRONMENT)/bin/activate; \
-	$(PYTHON) -m pip install --upgrade pip;               \
-	$(PYTHON) -m pip install -r $<;
+	$(PYTHON) -m venv $(PYTHON_VIRTUAL_ENVIRONMENT); \
+	$(call using-python-environment,                 \
+		$(PYTHON) -m pip install --upgrade pip;      \
+		$(PYTHON) -m pip install -r $<)
 	@$(TOUCH) $@
 
 $(VIRTUAL_ENVIRONMENT_DIR):
 	@$(MKDIR) $@
 
-# Usage: $(call using-python-enviroment, command[s])
-define using-python-environment
-	$(SOURCE) $(PYTHON_VIRTUAL_ENVIRONMENT)/bin/activate && ( $1 )
-endef
+DISTCLEAN_FILES += $(PYTHON_VIRTUAL_ENVIRONMENT).$(TS)
+DISTCLEAN_DIRS  += $(PYTHON_VIRTUAL_ENVIRONMENT)
