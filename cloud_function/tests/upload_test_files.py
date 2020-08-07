f2b9e526a39e8863a5ad8da75a4b8699e730146e
Saman Ehsan <sehsan@broadinstitute.org>
2020-08-07 14:17:21 -0400

    [GH-852] Parameterize bucket name in upload script (#91)
    * Parameterize bucket name in upload script
    
    * Update cloud_function/tests/upload_test_files.py
    
    Co-authored-by: Chengchen(Rex) Wang <14366016+rexwangcc@users.noreply.github.com>
    
    Co-authored-by: Chengchen(Rex) Wang <14366016+rexwangcc@users.noreply.github.com>

--- cloud_function/tests/upload_test_files.py
+++ cloud_function/tests/upload_test_files.py
@@ -1,51 +1,69 @@
 """ Helper script that copies all of the files for an arrays sample into the dev aou input bucket. This will trigger
 the submit_aou_workload cloud function for each file. When all files have been uploaded, it will launch an arrays
 workflow via the workflow launcher (but only if a workflow with that chipwell barcode & analysis version has not
-been run before). """
+been run before).
 
+Usage: python upload_test_files.py -b <bucket>
+"""
+
+import argparse
 import json
-import uuid
 import subprocess
 import tempfile
-
-bucket = "dev-aou-arrays-input"
-uuid = uuid.uuid4()
+import uuid
 
 arrays_path = "gs://broad-gotc-test-storage/arrays/HumanExome-12v1-1_A/"
 arrays_metadata_path = "gs://broad-gotc-test-storage/arrays/metadata/HumanExome-12v1-1_A/"
-dest_arrays_path = f"gs://{bucket}/{uuid}/v1/arrays/"
-dest_arrays_metadata_path = f"gs://{bucket}/{uuid}/v1/arrays/metadata/"
-dest_ptc_json_path = f"gs://{bucket}/{uuid}/v1/ptc.json"
 
-ptc_json = {
-    "cromwell": "https://cromwell-gotc-auth.gotc-dev.broadinstitute.org/",
-    "environment": "aou-dev",
-    "uuid": None,
-    "notifications": [{
-        "analysis_version_number": 1,
-        "chip_well_barcode": str(uuid),
-        "green_idat_cloud_path": f"gs://{bucket}/{uuid}/v1/arrays/HumanExome-12v1-1_A/idats/7991775143_R01C01/7991775143_R01C01_Grn.idat",
-        "params_file": f"gs://{bucket}/{uuid}/v1/arrays/HumanExome-12v1-1_A/inputs/7991775143_R01C01/params.txt",
-        "red_idat_cloud_path": f"gs://{bucket}/{uuid}/v1/arrays/HumanExome-12v1-1_A/idats/7991775143_R01C01/7991775143_R01C01_Red.idat",
-        "reported_gender": "Female",
-        "sample_alias": "NA12878",
-        "sample_lsid": "broadinstitute.org:bsp.dev.sample:NOTREAL.NA12878",
-        "bead_pool_manifest_file": f"gs://{bucket}/{uuid}/v1/arrays/metadata/HumanExome-12v1-1_A/HumanExome-12v1-1_A.bpm",
-        "cluster_file": f"gs://{bucket}/{uuid}/v1/arrays/metadata/HumanExome-12v1-1_A/HumanExomev1_1_CEPH_A.egt",
-        "zcall_thresholds_file": f"gs://{bucket}/{uuid}/v1/arrays/metadata/HumanExome-12v1-1_A/IBDPRISM_EX.egt.thresholds.txt",
-        "gender_cluster_file": f"gs://{bucket}/{uuid}/v1/arrays/metadata/HumanExome-12v1-1_A/HumanExomev1_1_gender.egt",
-        "extended_chip_manifest_file": f"gs://{bucket}/{uuid}/v1/arrays/metadata/HumanExome-12v1-1_A/HumanExome-12v1-1_A.1.3.extended.csv"
-    }]
-}
+def get_destination_paths(bucket, uuid):
+    return {
+        "arrays": f"gs://{bucket}/{uuid}/v1/arrays/",
+        "arrays_metadata": f"gs://{bucket}/{uuid}/v1/arrays/metadata/",
+        "ptc": f"gs://{bucket}/{uuid}/v1/ptc.json"
+    }
 
-def main():
+def get_ptc_json(bucket, uuid):
+    return {
+        "cromwell": "https://cromwell-gotc-auth.gotc-dev.broadinstitute.org/",
+        "environment": "aou-dev",
+        "uuid": None,
+        "notifications": [{
+            "analysis_version_number": 1,
+            "chip_well_barcode": str(uuid),
+            "green_idat_cloud_path": f"gs://{bucket}/{uuid}/v1/arrays/HumanExome-12v1-1_A/idats/7991775143_R01C01/7991775143_R01C01_Grn.idat",
+            "params_file": f"gs://{bucket}/{uuid}/v1/arrays/HumanExome-12v1-1_A/inputs/7991775143_R01C01/params.txt",
+            "red_idat_cloud_path": f"gs://{bucket}/{uuid}/v1/arrays/HumanExome-12v1-1_A/idats/7991775143_R01C01/7991775143_R01C01_Red.idat",
+            "reported_gender": "Female",
+            "sample_alias": "NA12878",
+            "sample_lsid": "broadinstitute.org:bsp.dev.sample:NOTREAL.NA12878",
+            "bead_pool_manifest_file": f"gs://{bucket}/{uuid}/v1/arrays/metadata/HumanExome-12v1-1_A/HumanExome-12v1-1_A.bpm",
+            "cluster_file": f"gs://{bucket}/{uuid}/v1/arrays/metadata/HumanExome-12v1-1_A/HumanExomev1_1_CEPH_A.egt",
+            "zcall_thresholds_file": f"gs://{bucket}/{uuid}/v1/arrays/metadata/HumanExome-12v1-1_A/IBDPRISM_EX.egt.thresholds.txt",
+            "gender_cluster_file": f"gs://{bucket}/{uuid}/v1/arrays/metadata/HumanExome-12v1-1_A/HumanExomev1_1_gender.egt",
+            "extended_chip_manifest_file": f"gs://{bucket}/{uuid}/v1/arrays/metadata/HumanExome-12v1-1_A/HumanExome-12v1-1_A.1.3.extended.csv"
+        }]
+    }
+
+def main(bucket):
+    id_ = uuid.uuid4()
+    ptc_json = get_ptc_json(bucket, id_)
+    destination_paths = get_destination_paths(bucket, id_)
     with tempfile.TemporaryDirectory() as tmpdirname:
         with open(f'{tmpdirname}/ptc.json', 'w') as f:
             json.dump(ptc_json, f)
-        subprocess.run(["gsutil", "cp", "-r", arrays_path, dest_arrays_path])
-        subprocess.run(["gsutil", "cp", "-r", arrays_metadata_path, dest_arrays_metadata_path])
-        subprocess.run(["gsutil", "cp", f"{tmpdirname}/ptc.json", dest_ptc_json_path])
+        subprocess.run(["gsutil", "cp", "-r", arrays_path, destination_paths["arrays"]])
+        subprocess.run(["gsutil", "cp", "-r", arrays_metadata_path, destination_paths["arrays_metadata"]])
+        subprocess.run(["gsutil", "cp", f"{tmpdirname}/ptc.json", destination_paths["ptc"]])
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-b",
+        "--bucket",
+        dest="bucket",
+        default="dev-aou-arrays-input",
+        help="The upload destination bucket."
+    )
+    args = parser.parse_args()
+    main(args.bucket)
