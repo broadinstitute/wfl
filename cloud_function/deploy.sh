2aa105a8f72fea47068252e7af52fe1a8fa623b3
Jack Warren <jack@jackwarren.info>
2020-08-10 14:45:06 -0400

    [GH-877] Change dev input bucket location

--- cloud_function/deploy.sh
+++ cloud_function/deploy.sh
@@ -6,10 +6,10 @@ TRIGGER_BUCKET=${2}
 TRIGGER_EVENT=${3:-"google.storage.object.finalize"}
 REGION=${4:-"us-central1"}
 
-if [ "${GCLOUD_PROJECT}" == "broad-gotc-dev" ]; then
+if [ "${GCLOUD_PROJECT}" == "broad-gotc-dev-storage" ]; then
   # This service account must have access to the WFL API
   SA_EMAIL="aou-cloud-fn-non-prod@broad-gotc-dev.iam.gserviceaccount.com"
-  _WFL_URL="https://workflow-launcher.gotc-dev.broadinstitute.org"
+  _WFL_URL="https://dev-wfl.gotc-dev.broadinstitute.org"
 elif [ "${GCLOUD_PROJECT}" == "broad-aou-storage" ]; then
   SA_EMAIL="aou-cloud-fn@broad-aou-storage.iam.gserviceaccount.com"
   _WFL_URL="https://aou-wfl.gotc-prod.broadinstitute.org"
