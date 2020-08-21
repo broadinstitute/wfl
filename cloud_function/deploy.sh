b037187b541c63e32cd79c36565b29502d1d2277
Saman Ehsan <sehsan@broadinstitute.org>
2020-08-21 18:37:32 -0400

    Add cromwell_url and wfl_environment env vars (#114)
    * Add cromwell_url and wfl_environment env vars
    
    * Override env-specific values in WFL payload

--- cloud_function/deploy.sh
+++ cloud_function/deploy.sh
@@ -10,9 +10,13 @@ if [ "${GCLOUD_PROJECT}" == "broad-gotc-dev-storage" ]; then
   # This service account must have access to the WFL API
   SA_EMAIL="aou-cloud-fn-non-prod@broad-gotc-dev-storage.iam.gserviceaccount.com"
   _WFL_URL="https://dev-wfl.gotc-dev.broadinstitute.org"
+  _CROMWELL_URL="https://cromwell-gotc-auth.gotc-dev.broadinstitute.org/"
+  _WFL_ENVIRONMENT="aou-dev"
 elif [ "${GCLOUD_PROJECT}" == "broad-aou-storage" ]; then
   SA_EMAIL="aou-cloud-fn@broad-aou-storage.iam.gserviceaccount.com"
   _WFL_URL="https://aou-wfl.gotc-prod.broadinstitute.org"
+  _CROMWELL_URL="https://cromwell-aou.gotc-prod.broadinstitute.org/"
+  _WFL_ENVIRONMENT="aou-prod"
 else
   printf "Unrecognized google project\n"
   exit 1
@@ -24,6 +28,6 @@ gcloud functions deploy submit_aou_workload \
     --trigger-resource ${TRIGGER_BUCKET} \
     --trigger-event ${TRIGGER_EVENT} \
     --service-account ${SA_EMAIL} \
-    --set-env-vars WFL_URL=${_WFL_URL} \
+    --set-env-vars WFL_URL=${_WFL_URL},CROMWELL_URL=${_CROMWELL_URL},WFL_ENVIRONMENT=${_WFL_ENVIRONMENT} \
     --runtime python37 \
     --memory 128MB
