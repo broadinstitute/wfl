0ab97f5cee37a9a6eb3600fb00175802cd07a9f5
Jack Warren <jack@jackwarren.info>
2020-10-08 14:36:35 -0400

    Add route to Swagger

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -45,6 +45,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "modules" */ '../views/Modules.vue')
   },
   {
+    path: '/swagger',
+    name: 'swagger',
+    // route level code-splitting
+    // this generates a separate chunk (modules.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "modules" */ '../views/Swagger.vue')
+  },
+  {
     path: '/error',
     name: 'error',
     component: () => import(/* webpackChunkName: "modules" */ '../views/Error.vue')
