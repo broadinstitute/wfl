53cff115ae42193a1602f75639551c22a8fbd8c6
Edmund Higham <edhigham@gmail.com>
2020-10-15 21:54:30 +0100

    prefix input keys

--- api/src/wfl/module/xx.clj
+++ api/src/wfl/module/xx.clj
@@ -1,21 +1,21 @@
 (ns wfl.module.xx
   "Reprocess External Exomes, whatever they are."
   (:require [clojure.data.json :as json]
+            [clojure.java.io :as io]
             [clojure.string :as str]
+            [clojure.tools.logging.readable :as logr]
+            [wfl.api.workloads :refer [defoverload]]
             [wfl.api.workloads :as workloads]
             [wfl.environments :as env]
             [wfl.jdbc :as jdbc]
             [wfl.module.all :as all]
             [wfl.service.gcs :as gcs]
             [wfl.service.postgres :as postgres]
-            [wfl.util :as util]
             [wfl.service.cromwell :as cromwell]
+            [wfl.util :as util]
             [wfl.wdl :as wdl]
-            [clojure.tools.logging.readable :as logr]
-            [clojure.java.io :as io]
             [wfl.wfl :as wfl])
-  (:import [java.time OffsetDateTime]
-           (java.util UUID)))
+  (:import [java.time OffsetDateTime]))
 
 (def pipeline "ExternalExomeReprocessing")
 
@@ -94,9 +94,10 @@
 (defn make-persisted-inputs [output-url common inputs]
   (let [sample-name (fn [basename] (first (str/split basename #"\.")))
         [_ path] (gcs/parse-gs-url (some inputs [:input_bam :input_cram]))
-        basename    (util/basename path)]
+        basename    (or (:base_file_name inputs) (util/basename path))]
     (->
       (util/deep-merge common inputs)
+      (assoc :base_file_name basename)
       (util/assoc-when util/absent? :sample_name (sample-name basename))
       (util/assoc-when util/absent? :final_gvcf_base_name basename)
       (util/assoc-when util/absent? :destination_cloud_path
@@ -118,8 +119,9 @@
 (defn submit-workload! [{:keys [uuid workflows] :as workload}]
   (letfn [(update-workflow [workflow cromwell-uuid]
             (assoc workflow :uuid cromwell-uuid
-                            :status "Submitted" ; we've just submitted it
-                            :updated (OffsetDateTime/now)))]
+                            :status "Submitted"             ; we've just submitted it
+                            :updated (OffsetDateTime/now)))
+          (add-prefix [inputs] (util/prefix-keys inputs (keyword pipeline)))]
     (let [path        (wdl/hack-unpack-resources-hack (:top workflow-wdl))
           environment (get-cromwell-environment workload)]
       (logr/infof "submitting workload %s" uuid)
@@ -129,7 +131,7 @@
           environment
           (io/file (:dir path) (path ".wdl"))
           (io/file (:dir path) (path ".zip"))
-          (map :inputs workflows)
+          (map (comp add-prefix :inputs) workflows)
           (util/make-options environment)
           (merge cromwell-labels {:workload uuid}))))))
 
@@ -171,141 +173,5 @@
       (assoc workload :workflows)
       unnilify)))
 
-(workloads/defoverload
-  workloads/create-workload!
-  pipeline
-  create-xx-workload!)
-
-(workloads/defoverload
-  workloads/start-workload!
-  pipeline
-  start-xx-workload!)
-
-;(def cromwell-label-map
-;  "The WDL label applied to Cromwell metadata."
-;  {(keyword (str wfl/the-name "-xx"))
-;   (wdl/workflow-name (:top workflow-wdl))})
-;
-;(def cromwell-label
-;  "The WDL label applied to Cromwell metadata."
-;  (let [[key value] (first cromwell-label-map)]
-;    (str (name key) ":" value)))
-;
-;(def references
-;  "HG38 reference, calling interval, and contamination files."
-;  (let [hg38ref "gs://gcp-public-data--broad-references/hg38/v0/"
-;        contam  "Homo_sapiens_assembly38.contam"
-;        calling "exome_calling_regions.v1"
-;        subset  (str hg38ref contam "." calling)]
-;    (merge references/hg38-exome-references
-;      {:calling_interval_list   (str hg38ref calling ".interval_list")
-;       :contamination_sites_bed (str subset ".bed")
-;       :contamination_sites_mu  (str subset ".mu")
-;       :contamination_sites_ud  (str subset ".UD")})))
-;
-;(def baits-and-targets
-;  "Baits and targets inputs."
-;  (let [private              "gs://broad-references-private/"
-;        bait_set_name        "whole_exome_illumina_coding_v1"
-;        interval_list        (str private "HybSelOligos/" bait_set_name "/" bait_set_name ".Homo_sapiens_assembly38")
-;        target_interval_list (str interval_list ".targets.interval_list")
-;        bait_interval_list   (str interval_list ".baits.interval_list")
-;        fasta                "gs://gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.fasta"]
-;    {:bait_set_name        bait_set_name
-;     :bait_interval_list   bait_interval_list
-;     :target_interval_list target_interval_list
-;     :cram_ref_fasta       fasta
-;     :cram_ref_fasta_index (str fasta ".fai")}))
-;
-;(defn make-inputs
-;  "Return inputs for reprocessing IN-GS into OUT-GS in ENVIRONMENT."
-;  [environment out-gs in-gs]
-;  (let [[input-key base _] (all/bam-or-cram? in-gs)
-;        leaf   (last (str/split base #"/"))
-;        [_ out-dir] (gcs/parse-gs-url (util/unsuffix base leaf))
-;        inputs (-> (zipmap [:base_file_name :final_gvcf_base_name :sample_name]
-;                     (repeat leaf))
-;                 (assoc input-key in-gs)
-;                 (assoc :destination_cloud_path (str out-gs out-dir))
-;                 (assoc :references references)
-;                 (merge (util/exome-inputs environment)
-;                   util/gatk-docker-inputs
-;                   util/gc_bias_metrics-inputs
-;                   baits-and-targets))
-;        {:keys [destination_cloud_path final_gvcf_base_name]} inputs
-;        output (str destination_cloud_path final_gvcf_base_name ".cram")]
-;    (all/throw-when-output-exists-already! output)
-;    (util/prefix-keys inputs :ExternalExomeReprocessing)))
-;
-;(defn active-objects
-;  "GCS object names of BAMs or CRAMs from IN-GS-URL now active in ENVIRONMENT."
-;  [environment in-gs-url]
-;  (prn (format "%s: querying Cromwell in %s" wfl/the-name environment))
-;  (let [md (partial cromwell/metadata environment)]
-;    (letfn [(active? [metadata]
-;              (let [url (-> metadata :id md :submittedFiles :inputs
-;                          (json/read-str :key-fn keyword)
-;                          (some [:ExternalExomeReprocessing.input_bam
-;                                 :ExternalExomeReprocessing.input_cram]))]
-;                (when url
-;                  (let [[bucket object] (gcs/parse-gs-url url)
-;                        [_ unsuffixed _] (all/bam-or-cram? object)
-;                        [in-bucket in-object] (gcs/parse-gs-url in-gs-url)]
-;                    (when (and (= in-bucket bucket)
-;                            (str/starts-with? object in-object))
-;                      unsuffixed)))))]
-;      (->> {:label  cromwell-label
-;            :status ["On Hold" "Running" "Submitted"]}
-;        (cromwell/query environment)
-;        (keep active?)
-;        set))))
-;
-;(defn hold-workflow
-;  "Hold OBJECT from IN-BUCKET for reprocessing into OUT-GS in
-;  ENVIRONMENT."
-;  [environment in-bucket out-gs object]
-;  (let [path  (wdl/hack-unpack-resources-hack (:top workflow-wdl))
-;        in-gs (gcs/gs-url in-bucket object)]
-;    (cromwell/hold-workflow
-;      environment
-;      (io/file (:dir path) (path ".wdl"))
-;      (io/file (:dir path) (path ".zip"))
-;      (make-inputs environment out-gs in-gs)
-;      (util/make-options environment)
-;      cromwell-label-map)
-;    (prn in-gs)))
-;
-;(defn on-hold-some-workflows
-;  "Submit 'On Hold' up to MAX workflows from IN-GS to OUT-GS
-;  in ENVIRONMENT."
-;  [environment max-string in-gs out-gs]
-;  (let [max    (util/is-non-negative! max-string)
-;        bucket (all/readable! in-gs)]
-;    (letfn [(input? [{:keys [name]}]
-;              (let [[_ unsuffixed suffix] (all/bam-or-cram? name)]
-;                (when unsuffixed [unsuffixed suffix])))]
-;      (let [done   (set/union (all/processed-crams (all/slashify out-gs))
-;                     (active-objects environment in-gs))
-;            suffix (->> in-gs
-;                     gcs/parse-gs-url
-;                     (apply gcs/list-objects)
-;                     (keep input?)
-;                     (into {}))
-;            more   (->> suffix
-;                     keys
-;                     (remove done)
-;                     (take max)
-;                     (map (fn [base] (str base (suffix base)))))]
-;        (run! (partial hold-workflow environment bucket out-gs) more)))))
-;
-;(defn release-some-on-hold-workflows
-;  "Submit up to MAX-STRING workflows from 'On Hold' in ENVIRONMENT."
-;  [environment max-string]
-;  (letfn [(release! [id] (prn (cromwell/release-hold environment id)))]
-;    (let [max (util/is-non-negative! max-string)]
-;      (->> {:pagesize max :status "On Hold" :label cromwell-label}
-;        (cromwell/query environment)
-;        (map :id)
-;        (take max)
-;        (run! release!)))))
-;
+(defoverload workloads/create-workload! pipeline create-xx-workload!)
+(defoverload workloads/start-workload! pipeline start-xx-workload!)
