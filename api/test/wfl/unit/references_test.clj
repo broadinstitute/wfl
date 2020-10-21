cbebc78f1344aaf4dafd04e4c0043ce0f34a40ac
Jack Warren <jack@jackwarren.info>
2020-10-21 16:46:01 -0400

    Add a test for reference_fasta fix

---
+++ api/test/wfl/unit/references_test.clj
@@ -0,0 +1,14 @@
+(ns wfl.unit.references-test
+  (:require [clojure.string :as str]
+            [clojure.test :refer [deftest testing is]]
+            [wfl.references :as references]))
+
+(deftest prefix-handling
+  (testing "reference_fasta function"
+    (testing "treats nil as no arg"
+      (is (= (references/reference_fasta) (references/reference_fasta nil))))
+    (testing "supplies a bucket prefix if no arg"
+      (run! #(is (str/starts-with? % "gs://")) (vals (references/reference_fasta))))
+    (testing "uses a given prefix"
+      (let [prefix "foo"]
+        (run! #(is (str/starts-with? % prefix)) (vals (references/reference_fasta prefix)))))))
