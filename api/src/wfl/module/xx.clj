562bb6b6ea1585f8a9bb97a8bec63d0a5a233ce5
Edmund Higham <edhigham@gmail.com>
2020-10-21 17:48:24 +0100

    make hg38 a function

--- api/src/wfl/module/xx.clj
+++ api/src/wfl/module/xx.clj
@@ -30,19 +30,19 @@
    :top     "pipelines/broad/reprocessing/external/exome/ExternalExomeReprocessing.wdl"})
 
 (def ^:private references-defaults
-  (let [hg38 "gs://gcp-public-data--broad-references/hg38/v0/"]
-    {:calling_interval_list      (str hg38 "exome_calling_regions.v1.interval_list")
-     :contamination_sites_bed    (str hg38 "contamination-resources/1000g/1000g.phase3.100k.b38.vcf.gz.dat.bed")
-     :contamination_sites_mu     (str hg38 "contamination-resources/1000g/1000g.phase3.100k.b38.vcf.gz.dat.mu")
-     :contamination_sites_ud     (str hg38 "contamination-resources/1000g/1000g.phase3.100k.b38.vcf.gz.dat.UD")
-     :dbsnp_vcf                  (str hg38 "Homo_sapiens_assembly38.dbsnp138.vcf")
-     :dbsnp_vcf_index            (str hg38 "Homo_sapiens_assembly38.dbsnp138.vcf.idx")
-     :evaluation_interval_list   (str hg38 "exome_evaluation_regions.v1.interval_list")
-     :haplotype_database_file    (str hg38 "Homo_sapiens_assembly38.haplotype_database.txt")
-     :known_indels_sites_vcfs    [(str hg38 "Mills_and_1000G_gold_standard.indels.hg38.vcf.gz")
-                                  (str hg38 "Homo_sapiens_assembly38.known_indels.vcf.gz")]
-     :known_indels_sites_indices [(str hg38 "Mills_and_1000G_gold_standard.indels.hg38.vcf.gz.tbi")
-                                  (str hg38 "Homo_sapiens_assembly38.known_indels.vcf.gz.tbi")]
+  (let [hg38 (partial str "gs://gcp-public-data--broad-references/hg38/v0/")]
+    {:calling_interval_list      (hg38 "exome_calling_regions.v1.interval_list")
+     :contamination_sites_bed    (hg38 "contamination-resources/1000g/1000g.phase3.100k.b38.vcf.gz.dat.bed")
+     :contamination_sites_mu     (hg38 "contamination-resources/1000g/1000g.phase3.100k.b38.vcf.gz.dat.mu")
+     :contamination_sites_ud     (hg38 "contamination-resources/1000g/1000g.phase3.100k.b38.vcf.gz.dat.UD")
+     :dbsnp_vcf                  (hg38 "Homo_sapiens_assembly38.dbsnp138.vcf")
+     :dbsnp_vcf_index            (hg38 "Homo_sapiens_assembly38.dbsnp138.vcf.idx")
+     :evaluation_interval_list   (hg38 "exome_evaluation_regions.v1.interval_list")
+     :haplotype_database_file    (hg38 "Homo_sapiens_assembly38.haplotype_database.txt")
+     :known_indels_sites_vcfs    [(hg38 "Mills_and_1000G_gold_standard.indels.hg38.vcf.gz")
+                                  (hg38 "Homo_sapiens_assembly38.known_indels.vcf.gz")]
+     :known_indels_sites_indices [(hg38 "Mills_and_1000G_gold_standard.indels.hg38.vcf.gz.tbi")
+                                  (hg38 "Homo_sapiens_assembly38.known_indels.vcf.gz.tbi")]
      :reference_fasta            (references/reference_fasta)}))
 
 (def ^:private scatter_settings-defaults
@@ -54,13 +54,13 @@
    :preemptible_tries     3})
 
 (def ^:private workflow-defaults
-  (let [hg38 "gs://gcp-public-data--broad-references/hg38/v0/"]
+  (let [hg38 (partial str "gs://gcp-public-data--broad-references/hg38/v0/")]
     {:unmapped_bam_suffix  ".unmapped.bam"
-     :cram_ref_fasta       (str hg38 "Homo_sapiens_assembly38.fasta")
-     :cram_ref_fasta_index (str hg38 "Homo_sapiens_assembly38.fasta.fai")
+     :cram_ref_fasta       (hg38 "Homo_sapiens_assembly38.fasta")
+     :cram_ref_fasta_index (hg38 "Homo_sapiens_assembly38.fasta.fai")
      :bait_set_name        "whole_exome_illumina_coding_v1"
-     :bait_interval_list   (str hg38 "HybSelOligos/whole_exome_illumina_coding_v1/whole_exome_illumina_coding_v1.Homo_sapiens_assembly38.baits.interval_list")
-     :target_interval_list (str hg38 "HybSelOligos/whole_exome_illumina_coding_v1/whole_exome_illumina_coding_v1.Homo_sapiens_assembly38.targets.interval_list")
+     :bait_interval_list   (hg38 "HybSelOligos/whole_exome_illumina_coding_v1/whole_exome_illumina_coding_v1.Homo_sapiens_assembly38.baits.interval_list")
+     :target_interval_list (hg38 "HybSelOligos/whole_exome_illumina_coding_v1/whole_exome_illumina_coding_v1.Homo_sapiens_assembly38.targets.interval_list")
      :references           references-defaults
      :scatter_settings     scatter_settings-defaults
      :papi_settings        papi_settings-defaults}))
