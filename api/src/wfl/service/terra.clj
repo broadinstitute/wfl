c0525f49c741e806b8097f31d482acabcf925c36
Saman Ehsan <se8ea@virginia.edu>
2020-10-22 16:59:07 -0400

    Clean-up terra module

--- api/src/wfl/service/terra.clj
+++ api/src/wfl/service/terra.clj
@@ -3,7 +3,8 @@
   (:require [clojure.data.json :as json]
             [clojure.string :as str]
             [clj-http.client :as http]
-            [wfl.once :as once]))
+            [wfl.once :as once]
+            [wfl.util :as util]))
 
 (defn workspace-api-url
   [terra-url workspace-namespace workspace-name]
@@ -13,41 +14,30 @@
   "Submit samples in a workspace for analysis with a method configuration in Terra."
   [terra-url workspace-namespace workspace-name method-configuration-name
    method-configuration-namespace entity-type entity-name]
-  (let [workspace-url (workspace-api-url terra-url workspace-namespace workspace-name)]
-    (-> {:method    :post
-         :url       (str workspace-url "/submissions")
-         :content-type :application/json
+  (let [workspace-url (workspace-api-url terra-url workspace-namespace workspace-name)
+        submission-url (str workspace-url "/submissions")]
+    (->
+      (http/post
+        submission-url
+        {:content-type :application/json
          :headers   (once/get-auth-header)
-         :body      (json/write-str {
-                    :methodConfigurationNamespace method-configuration-namespace
-                    :methodConfigurationName method-configuration-name
-                    :entityType entity-type
-                    :entityName entity-name
-                    :useCallCache true
-                   } :escape-slash false)}
-      http/request :body
-      (json/read-str :key-fn keyword)
+         :body      (json/write-str
+                      {
+                       :methodConfigurationNamespace method-configuration-namespace
+                       :methodConfigurationName method-configuration-name
+                       :entityType entity-type
+                       :entityName entity-name
+                       :useCallCache true
+                       } :escape-slash false)})
+      :body
+      (util/parse-json)
       :submissionId)))
 
 (defn get-submission
   "Get information about a Terra Cromwell submission."
   [terra-url workspace-namespace workspace-name submission-id]
-  (let [workspace-url (workspace-api-url terra-url workspace-namespace workspace-name)]
-    (-> {:method  :get
-         :url     (str workspace-url "/submissions/" submission-id)
-         :headers (once/get-auth-header)}
-      http/request :body
-      (json/read-str :key-fn keyword))))
+  (let [workspace-url (workspace-api-url terra-url workspace-namespace workspace-name)
+        submission-url (str workspace-url "/submissions/" submission-id)
+        response (http/get submission-url {:headers (once/get-auth-header)})]
+      (util/parse-json (:body response))))
 
-(comment
-  (let [terra-url "https://firecloud-orchestration.dsde-dev.broadinstitute.org"
-        workspace-namespace "general-dev-billing-account"
-        workspace-name "wfl-integration"
-        method-configuration-name "ExternalWholeGenomeReprocessing"
-        method-configuration-namespace "general-dev-billing-account"
-        entity-type "sample"
-        entity-name "NA12878_PLUMBING"
-        submission-id "933129e1-4945-4ad5-9fd5-933d0a70f21c"]
-    (create-submission terra-url workspace-namespace workspace-name method-configuration-name
-                       method-configuration-namespace entity-type entity-name)
-    (get-submission terra-url workspace-namespace workspace-name submission-id)))
