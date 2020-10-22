f701af4e8bce349c500f68b012ae5d51b8ac78d0
Tom Lyons <tbl3rd@gmail.com>
2020-10-22 15:36:12 -0400

    Maybe a better reference_fasta?

--- api/src/wfl/references.clj
+++ api/src/wfl/references.clj
@@ -2,20 +2,18 @@
   "Define shared static references.")
 
 (defn reference_fasta
-  "Default value for references.reference_fastas."
-  ([prefix]
-   {:ref_dict        (str prefix ".dict")
-    :ref_fasta       (str prefix ".fasta")
-    :ref_fasta_index (str prefix ".fasta.fai")
-    :ref_ann         (str prefix ".fasta.64.ann")
-    :ref_bwt         (str prefix ".fasta.64.bwt")
-    :ref_pac         (str prefix ".fasta.64.pac")
-    :ref_alt         (str prefix ".fasta.64.alt")
-    :ref_amb         (str prefix ".fasta.64.amb")
-    :ref_sa          (str prefix ".fasta.64.sa")})
-  ([]
-   (reference_fasta
-     "gs://gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38")))
+  "Default value for references.reference_fasta with PREFIX."
+  [prefix]
+  {:pre [(string? prefix) (seq prefix)]}
+  {:ref_dict        (str prefix ".dict")
+   :ref_fasta       (str prefix ".fasta")
+   :ref_fasta_index (str prefix ".fasta.fai")
+   :ref_ann         (str prefix ".fasta.64.ann")
+   :ref_bwt         (str prefix ".fasta.64.bwt")
+   :ref_pac         (str prefix ".fasta.64.pac")
+   :ref_alt         (str prefix ".fasta.64.alt")
+   :ref_amb         (str prefix ".fasta.64.amb")
+   :ref_sa          (str prefix ".fasta.64.sa")})
 
 (defn hg38-genome-references
   "HG38 reference files for genome reprocessing."
@@ -23,7 +21,8 @@
   (let [gold    "Mills_and_1000G_gold_standard.indels.hg38"
         hsa     "Homo_sapiens_assembly38"
         regions "_regions.hg38.interval_list"
-        hg      (partial str "gs://gcp-public-data--broad-references/hg38/v0/")]
+        hg      (partial str "gs://gcp-public-data--broad-references/hg38/v0/")
+        r_f_p   (fnil reference_fasta (hg hsa))]
     {:haplotype_scatter_count     10
      :break_bands_at_multiples_of 100000
      :haplotype_database_file     (hg hsa ".haplotype_database.txt")
@@ -34,7 +33,7 @@
                                   (hg hsa  ".known_indels.vcf.gz.tbi")]
      :known_indels_sites_vcfs    [(hg gold ".vcf.gz")
                                   (hg hsa  ".known_indels.vcf.gz")]
-     :reference_fasta             (if prefix (reference_fasta prefix) (reference_fasta))}))
+     :reference_fasta             (r_f_p prefix)}))
 
 (def hg38-exome-references
   "HG38 reference files for exome reprocessing."
@@ -54,7 +53,7 @@
                                   (hg hsa  ".known_indels.vcf.gz.tbi")]
      :known_indels_sites_vcfs    [(hg gold ".vcf.gz")
                                   (hg hsa  ".known_indels.vcf.gz")]
-     :reference_fasta             (reference_fasta)}))
+     :reference_fasta             (reference_fasta (hg hsa))}))
 
 (def hg19-arrays-references
   "HG19 reference files for arrays processing."
