d29fae3aadbd2d34a1e557782ef1dcd6cf90b672
Jack Warren <jack@jackwarren.info>
2020-10-28 15:49:43 -0400

    Add xx integration test

--- api/test/wfl/integration/modules/xx_test.clj
+++ api/test/wfl/integration/modules/xx_test.clj
@@ -1,6 +1,6 @@
 (ns wfl.integration.modules.xx-test
   (:require [clojure.test :refer [deftest testing is] :as clj-test]
-            [wfl.service.cromwell :refer [wait-for-workflow-complete]]
+            [wfl.service.cromwell :refer [wait-for-workflow-complete submit-workflows]]
             [wfl.tools.endpoints :as endpoints]
             [wfl.tools.fixtures :as fixtures]
             [wfl.tools.workloads :as workloads]
@@ -82,3 +82,36 @@
                    workloads/execute-workload!
                    workloads/update-workload!)]
     (is (:finished workload))))
+
+(deftest test-workflow-options
+  (let [option-sequence [:a :a :b]
+        workload-request (-> (make-xx-workload-request)
+                             (update :items (fn [existing]
+                                              (mapv #(assoc %1 :workflow_options {%2 "some value"})
+                                                    (repeat (first existing)) option-sequence)))
+                             (update :workflow_options (fn [_] {:c "some other value"})))
+        submitted-option-counts (atom {})
+        ;; Mock cromwell/submit-workflows (note the plural), count observed option keys per workflow
+        pretend-submit (fn [_ _ _ inputs options _]
+                         (run! #(swap! submitted-option-counts update % (fnil (partial + (count inputs)) 0))
+                               (keys options))
+                         (repeatedly (count inputs) #(str (UUID/randomUUID))))]
+    (with-redefs-fn {#'submit-workflows pretend-submit}
+      #(-> workload-request
+           workloads/execute-workload!
+           (as-> workload
+                 (testing "Options in server response"
+                   (is (:workflow_options workload))
+                   (is (= (count option-sequence)
+                          (count (filter (fn [w] (get-in w [:workflow_options :c])) (:workflows workload)))))
+                   (is (= (count (filter (partial = :a) option-sequence))
+                          (count (filter (fn [w] (get-in w [:workflow_options :a])) (:workflows workload)))))
+                   (is (= (count (filter (partial = :b) option-sequence))
+                          (count (filter (fn [w] (get-in w [:workflow_options :b])) (:workflows workload)))))))))
+    (testing "Options sent to Cromwell"
+      (is (= (count option-sequence)
+             (:c @submitted-option-counts)))
+      (is (= (count (filter (partial = :a) option-sequence))
+             (:a @submitted-option-counts)))
+      (is (= (count (filter (partial = :b) option-sequence))
+             (:b @submitted-option-counts))))))
