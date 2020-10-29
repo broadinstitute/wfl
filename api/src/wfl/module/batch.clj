6da4882bbe2e34fcdbc9ec45baa5def5be276d35
Jack Warren <jack@jackwarren.info>
2020-10-29 16:00:52 -0400

    Merge

--- api/src/wfl/module/batch.clj
+++ api/src/wfl/module/batch.clj
@@ -14,24 +14,24 @@
   ([tx workflow-wdl workload-request]
    (add-workload-table! tx workflow-wdl workload-request {}))
   ([tx
-   {:keys [release top] :as _workflow-wdl}
-   {:keys [pipeline] :as workload-request}
-   default-workflow-options]
-  (let [workflow-options (util/deep-merge default-workflow-options (:workflow_options workload-request))
-        [{:keys [id]}]
-        (-> workload-request
-          (select-keys [:creator :cromwell :input :output :project])
-          (merge (-> (wfl/get-the-version) (select-keys [:commit :version])))
-          (assoc :release release
-                 :wdl top
-                 :uuid (UUID/randomUUID)
-                 :workflow_options (json/write-str workflow-options))
-          (->> (jdbc/insert! tx :workload)))
-        table (format "%s_%09d" pipeline id)]
-    (jdbc/execute! tx
-      ["UPDATE workload SET pipeline = ?::pipeline WHERE id = ?" pipeline id])
-    (jdbc/db-do-commands tx
-      (map #(format "CREATE TABLE %s OF CromwellWorkflow (PRIMARY KEY (id))" %)
-        [table]))
-    (jdbc/update! tx :workload {:items table} ["id = ?" id])
-    [id table workflow-options])))
+    {:keys [release top] :as _workflow-wdl}
+    {:keys [pipeline] :as workload-request}
+    default-workflow-options]
+   (let [workflow-options (util/deep-merge default-workflow-options (:workflow_options workload-request))
+         [{:keys [id]}]
+         (-> workload-request
+             (select-keys [:creator :cromwell :input :output :project])
+             (merge (-> (wfl/get-the-version) (select-keys [:commit :version])))
+             (assoc :release release
+                    :wdl top
+                    :uuid (UUID/randomUUID)
+                    :workflow_options (json/write-str workflow-options))
+             (->> (jdbc/insert! tx :workload)))
+         table (format "%s_%09d" pipeline id)]
+     (jdbc/execute! tx
+                    ["UPDATE workload SET pipeline = ?::pipeline WHERE id = ?" pipeline id])
+     (jdbc/db-do-commands tx
+                          (map #(format "CREATE TABLE %s OF CromwellWorkflow (PRIMARY KEY (id))" %)
+                               [table]))
+     (jdbc/update! tx :workload {:items table} ["id = ?" id])
+     [id table workflow-options])))
