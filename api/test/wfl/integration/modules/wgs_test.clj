f539e38a0c82b085d8637ecba2ab25a3e15cc238
Edmund Higham <edhigham@gmail.com>
2020-10-30 15:16:22 +0000

    add test for specifying input_bam

--- api/test/wfl/integration/modules/wgs_test.clj
+++ api/test/wfl/integration/modules/wgs_test.clj
@@ -1,12 +1,15 @@
 (ns wfl.integration.modules.wgs-test
-  (:require [clojure.test :refer [deftest testing is] :as clj-test]
+  (:require [clojure.set :refer [rename-keys]]
+            [clojure.test :refer [deftest testing is] :as clj-test]
             [wfl.service.cromwell :refer [wait-for-workflow-complete]]
             [wfl.tools.endpoints :as endpoints]
             [wfl.tools.fixtures :as fixtures]
             [wfl.tools.workloads :as workloads]
             [wfl.module.wgs :as wgs]
             [wfl.jdbc :as jdbc]
-            [wfl.module.all :as all])
+            [wfl.module.all :as all]
+            [clojure.string :as str]
+            [wfl.util :as util])
   (:import (java.util UUID)))
 
 (clj-test/use-fixtures :once fixtures/temporary-postgresql-database)
@@ -37,7 +40,7 @@
   (let [request (make-wgs-workload-request)]
     (jdbc/with-db-transaction [tx (fixtures/testing-db-config)]
       (let [[uuid table] (all/add-workload-table! tx wgs/workflow-wdl request)
-            add-id       (fn [m id] (assoc (:inputs m) :id id))]
+            add-id (fn [m id] (assoc (:inputs m) :id id))]
         (jdbc/insert-multi! tx table (map add-id (:items request) (range)))
         (jdbc/update! tx :workload {:version "0.3.8"} ["uuid = ?" uuid])
         uuid))))
@@ -48,3 +51,29 @@
       (let [workload (workloads/load-workload-for-uuid uuid)]
         (is (= uuid (:uuid workload)))
         (is (= wgs/pipeline (:pipeline workload)))))))
+
+(deftest test-exec-with-input_bam
+  (letfn [(go! [workflow]
+            (is (:uuid workflow))
+            (is (:status workflow))
+            (is (:updated workflow)))
+          (use-input_bam [item]
+            (update item :inputs
+              #(-> %
+                 (dissoc :input_cram)
+                 (assoc :input_bam "gs://inputs/fake.bam"))))
+          (verify-use_input_bam! [env in out inputs labels]
+            (is (str/ends-with? in ".bam"))
+            (is (contains? inputs :input_bam))
+            (is (util/absent? inputs :input_cram))
+            (is (contains? labels :workload))
+            [env in out inputs labels])]
+    (with-redefs-fn
+      {#'wgs/really-submit-one-workflow
+       (comp mock-really-submit-one-workflow verify-use_input_bam!)}
+      #(-> (make-wgs-workload-request)
+         (update :items (comp vector use-input_bam first))
+         (workloads/execute-workload!)
+         (as-> workload
+           (is (:started workload))
+           (run! go! (:workflows workload)))))))
