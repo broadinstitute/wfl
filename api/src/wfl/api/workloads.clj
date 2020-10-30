f38038343a3defdaf1a248606f622f485f58a189
Jack Warren <jack@jackwarren.info>
2020-10-30 16:12:44 -0400

    Fix default loader post-merge (aou and copyfile)

--- api/src/wfl/api/workloads.clj
+++ api/src/wfl/api/workloads.clj
@@ -1,6 +1,7 @@
 (ns wfl.api.workloads
   (:require [wfl.service.postgres :as postgres]
-            [wfl.jdbc :as jdbc]))
+            [wfl.jdbc :as jdbc]
+            [wfl.util :as util]))
 
 ;; always derive from base :wfl/exception
 (derive ::invalid-pipeline :wfl/exception)
@@ -109,13 +110,16 @@
   [tx workload]
   (letfn [(unnilify [m] (into {} (filter second m)))
           (split-inputs [m]
-            (let [keep [:id :finished :status :updated :uuid]]
+            (let [keep [:id :finished :status :updated :uuid :workflow_options]]
               (assoc (select-keys m keep)
-                :inputs (unnilify (apply dissoc m keep)))))]
+                :inputs (unnilify (apply dissoc m keep)))))
+          (unpack-options [m]
+            (update m :workflow_options #(when % (util/parse-json %))))]
     (try
       (->> (postgres/get-table tx (:items workload))
-        (mapv (comp unnilify split-inputs))
+        (mapv (comp unnilify split-inputs unpack-options))
         (assoc workload :workflows)
+        unpack-options
         unnilify)
       (catch Throwable cause
         (throw (ex-info "Error loading workload"
