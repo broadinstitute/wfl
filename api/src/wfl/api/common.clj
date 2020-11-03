8763cb8aa93511ef4f8654804c491d6292e78e46
Jack Warren <jack@jackwarren.info>
2020-11-03 16:24:30 -0500

    Add comments

--- api/src/wfl/api/common.clj
+++ api/src/wfl/api/common.clj
@@ -1,11 +1,11 @@
 (ns wfl.api.common
   "API support for workflow content being partially common across a workload."
-  (:require [wfl.service.postgres :as postgres]
-            [wfl.jdbc :as jdbc]
+  (:require [wfl.jdbc :as jdbc]
             [wfl.util :as util]
             [clojure.data.json :as json]))
 
 (defn store-common
+  "Store CONTENT to be common across the entire workload."
   [tx workload-id content]
   (jdbc/insert! tx :common_within_workload
                 (-> content
@@ -15,12 +15,14 @@
                     (assoc :workload_id workload-id))))
 
 (defn ^:private unnilify
+  "A recursive no-keys-for-nil-or-empty-values helper."
   [m]
   (if (map? m)
     (not-empty (into {} (filter second (map (fn [[k v]] [k (unnilify v)]) m))))
     m))
 
 (defn ^:private read-common
+  "The opposite of [[store-common]], simply reads the content back out, [[unnilify]]ed."
   [tx workload-id]
   (let [common (jdbc/query tx ["SELECT * FROM common_within_workload WHERE workload_id = ?" workload-id])]
     (->> (dissoc (first common) :workload_id)
@@ -29,6 +31,7 @@
          unnilify)))
 
 (defn load-common-into-workload
+  "Put any common content back into WORKLOAD, making it the baseline for each workflow item."
   [tx workload]
   (if-let [content (read-common tx (:id workload))]
     (-> workload
