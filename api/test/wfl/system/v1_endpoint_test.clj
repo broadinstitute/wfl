c2b179e96c22f1cdb54bb79ed67b394da7dd3509
Jack Warren <jack@jackwarren.info>
2020-11-05 15:55:55 -0500

    Revert "Just a first draft"
    This reverts commit c029c2bf

--- api/test/wfl/system/v1_endpoint_test.clj
+++ api/test/wfl/system/v1_endpoint_test.clj
@@ -31,14 +31,14 @@
 (defn- verify-succeeded-workload [workload]
   (run! verify-succeeded-workflow (:workflows workload)))
 
-(defn test-oauth2-endpoint []
+(deftest test-oauth2-endpoint
   (testing "The `oauth2_id` endpoint indeed provides an ID"
     (let [response (endpoints/get-oauth2-id)]
       (is (= (count response) 2))
       (is (some #(= % :oauth2-client-id) response))
       (is (some #(str/includes? % "apps.googleusercontent.com") response)))))
 
-(defn test-create-workload [r]
+(deftest test-create-workload
   (letfn [(go! [uuids {:keys [pipeline] :as request}]
             (testing (format "calling api/v1/create with %s workload request" pipeline)
               (let [{:keys [uuid] :as workload} (endpoints/create-workload request)]
@@ -50,9 +50,16 @@
                 (let [include [:pipeline :cromwell :project]]
                   (is (= (select-keys request include) (select-keys workload include))))
                 (conj uuids uuid))))]
-    (go! (get-existing-workload-uuids) r)))
+    (reduce go!
+      (get-existing-workload-uuids)
+      [(workloads/wgs-workload-request (UUID/randomUUID))
+       (workloads/aou-workload-request (UUID/randomUUID))
+       (workloads/xx-workload-request (UUID/randomUUID))
+       (workloads/copyfile-workload-request
+         "gs://fake-inputs/lolcats.txt"
+         "gs://fake-outputs/copied.txt")])))
 
-(defn test-start-workload [w]
+(deftest test-start-workload
   (letfn [(go! [{:keys [uuid pipeline] :as workload}]
             (testing (format "calling api/v1/start with %s workload" pipeline)
               (let [workload (endpoints/start-workload workload)]
@@ -62,9 +69,17 @@
                   (is (every? :updated workflows))
                   (is (every? :uuid workflows)))
                 (workloads/when-done verify-succeeded-workload workload))))]
-    (go! w)))
+    (with-temporary-gcs-folder uri
+      (let [src (str uri "input.txt")]
+        (-> (str/join "/" ["test" "resources" "copy-me.txt"])
+          (gcs/upload-file src))
+        (run! go!
+          [(create-wgs-workload)
+           (create-aou-workload)
+           (create-xx-workload)
+           (create-copyfile-workload src (str uri "output.txt"))])))))
 
-(defn test-exec-workload [r]
+(deftest test-exec-workload
   (letfn [(go! [uuids {:keys [pipeline] :as request}]
             (testing (format "calling api/v1/exec with %s workload request" pipeline)
               (let [{:keys [uuid] :as workload} (endpoints/exec-workload request)]
@@ -80,9 +95,18 @@
                   (is (every? :uuid workflows)))
                 (workloads/when-done verify-succeeded-workload workload)
                 (conj uuids uuid))))]
-    (go! (get-existing-workload-uuids) r)))
+    (with-temporary-gcs-folder uri
+      (let [src (str uri "input.txt")]
+        (-> (str/join "/" ["test" "resources" "copy-me.txt"])
+          (gcs/upload-file src))
+        (reduce go!
+          (get-existing-workload-uuids)
+          [(workloads/wgs-workload-request (UUID/randomUUID))
+           (workloads/aou-workload-request (UUID/randomUUID))
+           (workloads/xx-workload-request (UUID/randomUUID))
+           (workloads/copyfile-workload-request src (str uri "output.txt"))])))))
 
-(defn test-append-to-aou-workload []
+(deftest test-append-to-aou-workload
   (let [await    (partial cromwell/wait-for-workflow-complete :aou-dev)
         workload (endpoints/exec-workload
                    (workloads/aou-workload-request (UUID/randomUUID)))]
@@ -94,51 +118,10 @@
       (->> (endpoints/get-workload-status (:uuid workload))
         (workloads/when-done verify-succeeded-workload)))))
 
-(defn test-bad-pipeline []
+(deftest test-bad-pipeline
   (let [request (-> (workloads/copyfile-workload-request "gs://fake/in" "gs://fake/out")
                   (assoc :pipeline "geoff"))]
     (testing "create-workload! fails with bad request"
       (is (thrown? ExceptionInfo (endpoints/create-workload request))))
     (testing "create-workload! fails with bad request"
       (is (thrown? ExceptionInfo (endpoints/exec-workload request))))))
-
-
-(deftest parallel-tests
-  (with-temporary-gcs-folder uri
-    (let [src (str uri "input.txt")
-          _ (-> (str/join "/" ["test" "resources" "copy-me.txt"])
-                (gcs/upload-file src))
-          requests (fn [] [(workloads/wgs-workload-request (UUID/randomUUID))
-                           (workloads/aou-workload-request (UUID/randomUUID))
-                           (workloads/xx-workload-request (UUID/randomUUID))
-                           (workloads/copyfile-workload-request src (str uri "output.txt"))])
-          workloads (fn [] [(create-wgs-workload)
-                            (create-aou-workload)
-                            (create-xx-workload)
-                            (create-copyfile-workload src (str uri "output.txt"))])
-          test-fns (concat
-                     (map #(partial test-exec-workload %) requests)
-                     [test-oauth2-endpoint
-                      test-append-to-aou-workload
-                      test-bad-pipeline])
-          futures (doall (map future-call test-fns))]
-      (run! deref futures))))
-
-
-(deftest parallel-test-different
-  (with-temporary-gcs-folder uri
-    (let [create-requests [(workloads/wgs-workload-request (UUID/randomUUID))
-                           (workloads/aou-workload-request (UUID/randomUUID))
-                           (workloads/xx-workload-request (UUID/randomUUID))]
-          create-futures (doall (map #(future (endpoints/create-workload %))
-                                     create-requests))
-          create-responses (map deref create-futures)]
-      (letfn [(test-create-response [[{:keys [pipeline] :as request} {:keys [uuid] :as workload}]]
-                     (testing (format "calling api/v1/create with %s workload request" pipeline)
-                         (is uuid "workloads should be been assigned a uuid")
-                         (is (:created workload) "should have a created timestamp")
-                         (is (= (:email @endpoints/userinfo) (:creator workload)) "creator inferred from auth token")
-                         (is (not (:started workload)) "hasn't been started in cromwell")
-                         (let [include [:pipeline :cromwell :project]]
-                           (is (= (select-keys request include) (select-keys workload include))))))]
-        (run! test-create-response (map list create-requests create-responses))))))
