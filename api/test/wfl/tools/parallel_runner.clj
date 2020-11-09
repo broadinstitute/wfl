3a9f075ace793992d1f8ee0de0306c3ac0526194
Jack Warren <jack@jackwarren.info>
2020-11-09 12:28:29 -0500

    Patch up the runner
    - back to java 8 courtesy of tika (that we already have? :shrug:)
    - suppress logging (thank you binding)
    - support multiple namespaces (it isn't any longer so :shrug:)
    - usage comment

--- api/test/wfl/tools/parallel_runner.clj
+++ api/test/wfl/tools/parallel_runner.clj
@@ -1,23 +1,31 @@
 (ns wfl.tools.parallel-runner
-  (:require [clojure.test :as test])
-  (:import (java.io OutputStreamWriter StringWriter OutputStream)))
+  (:require [clojure.test :as test]
+            [clojure.tools.logging :as log]
+            [clojure.tools.logging.impl :refer [disabled-logger-factory]])
+  (:import (java.io OutputStreamWriter StringWriter)
+           (org.apache.tika.io NullOutputStream)))
 
 (defn -main
+  "Load given namespaces and run tests marked with ^:parallel in concurrent threads.
+
+  Does not support fixtures. Will run tests without ^:parallel sequentially.
+  Output will be held until all threads complete."
   [& args]
   (letfn [(go! [& tests]
-            (let [null   (new OutputStreamWriter (OutputStream/nullOutputStream)) ;; Java 11+
+            (let [null   (new OutputStreamWriter (NullOutputStream/nullOutputStream))
                   output (new StringWriter)]
-              (binding [*out* null
-                        test/*test-out* output
-                        test/*report-counters* (ref test/*initial-report-counters*)]
+              (binding [*out*                  null
+                        test/*test-out*        output
+                        test/*report-counters* (ref test/*initial-report-counters*)
+                        log/*logger-factory*   disabled-logger-factory]
                 (doseq [t tests] (t))
                 [@test/*report-counters* (str output)])))]
-    (let [ns      (symbol (first args))
-          _       (use ns)
-          tests   (group-by (comp #(contains? % :parallel) meta)
-                            (filter (comp :test meta) (vals (ns-interns (the-ns ns)))))
-          futures (doall (map #(future (go! %)) (tests true)))
-          results (apply list (apply go! (tests false)) (map deref futures))
+    (let [ns      (map symbol args)
+          _       (run! use ns)
+          tests   (filter (comp :test meta) (apply concat (map (comp vals ns-interns the-ns) ns)))
+          grouped (group-by (comp #(contains? % :parallel) meta) tests)
+          futures (doall (map #(future (go! %)) (grouped true)))
+          results (apply list (apply go! (grouped false)) (map deref futures))
           summary (assoc (apply merge-with + (map first results))
                     :type :summary)]
       (run! println (filter not-empty (map second results)))
