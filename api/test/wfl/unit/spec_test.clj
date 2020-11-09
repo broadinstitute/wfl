f798e9f7079982bb48fabe920636e8ea35d11b69
Rex <rexwangcc@gmail.com>
2020-11-09 13:42:24 -0500

    Small test.

--- api/test/wfl/unit/spec_test.clj
+++ api/test/wfl/unit/spec_test.clj
@@ -2,7 +2,8 @@
   (:require [clojure.test :refer [deftest testing is]]
             [wfl.tools.workloads :as workloads]
             [wfl.api.spec :as spec]
-            [clojure.spec.alpha :as s])
+            [clojure.spec.alpha :as s]
+            [clojure.walk :as walk])
   (:import (java.util UUID)))
 
 (deftest request-spec-test
@@ -11,4 +12,12 @@
       (run! valid? (map #(% (UUID/randomUUID))
                         [workloads/wgs-workload-request
                          workloads/aou-workload-request
-                         workloads/xx-workload-request])))))
+                         workloads/xx-workload-request]))))
+  (testing "Mismatched cram/bam inputs cannot pass spec validation"
+    (letfn [(valid? [req] (is (not (s/valid? ::spec/workload-request req))))]
+      (let [requests (map #(% (UUID/randomUUID))
+                          [workloads/wgs-workload-request
+                           workloads/xx-workload-request])
+            mismatched-requests (map #(walk/postwalk-replace {:input_cram :input_bam} %)
+                                     requests)]
+        (run! valid? mismatched-requests)))))
