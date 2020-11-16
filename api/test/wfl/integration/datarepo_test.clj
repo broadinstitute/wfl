1709fcb16069b61a3e7893f536fa91cb1a94f49d
Tom Lyons <tbl3rd@gmail.com>
2020-11-16 18:47:25 -0500

    :sigh:

--- api/test/wfl/integration/datarepo_test.clj
+++ api/test/wfl/integration/datarepo_test.clj
@@ -38,10 +38,10 @@
           (stage vcf "bogus vcf content")
           (stage (str vcf ".tbi") "bogus index content")
           (stage table (json/write-str
-                         {:id        object
-                          :vcf       (ingest vcf-url vcf)
-                          :vcf_index (ingest vcf-url (str vcf ".tbi"))}
-                         :escape-slash false))
+                        {:id        object
+                         :vcf       (ingest vcf-url vcf)
+                         :vcf_index (ingest vcf-url (str vcf ".tbi"))}
+                        :escape-slash false))
           (let [table-url (gcs/gs-url bucket (str object table))
                 job (datarepo/tabular-ingest :gotc-dev dataset table-url "sample")
                 {:keys [bad_row_count row_count]} (datarepo/poll-job :gotc-dev job)]
