dfc494564a38ca7ce23bf2d6f54885347a39a0e4
Jack Warren <jack@jackwarren.info>
2020-11-30 11:43:14 -0500

    [GH-1080] Use updated environments.clj with gotc-prod being wgs-prod (#250)
    * Include gotc-prod jes_roots
    
    * Use wgs-prod

--- api/src/wfl/boot.clj
+++ api/src/wfl/boot.clj
@@ -97,7 +97,7 @@
     (let [environments (clone "pipeline-config" "wfl/environments.clj")]
       (stage resources (clone "warp" "tasks/broad/CopyFilesFromCloudToCloud.wdl"))
       (util/shell-io! "git" "-C" (.getParent environments)
-        "checkout" "725f0ce4a1afa8db9c153ef2e1ba17bf1d89eb1f")
+        "checkout" "3f182c0b06ee5f2dfebf15ed8b12d513027878ae")
       (stage sources environments))))
 
 ;; Hack: (delete-tree directory) is a hack.
