45fc0b36192dd97febc6e1d6ab2301eef14129c0
Jack Warren <jack@jackwarren.info>
2020-12-21 14:29:10 -0500

    Add a mocking test

--- functions/sg/tests/unit_tests.py
+++ functions/sg/tests/unit_tests.py
@@ -1,5 +1,7 @@
 import os
-import re
+import mock
+import json
+import pytest
 from sg import main
 
 
@@ -61,3 +63,43 @@ def test_describe_workload():
         ]
     }
     assert main.describe_workload(workload_one) == ['bar', 'baz']
+
+
+def mocked_requests_post(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, text, status_code):
+            self.text = text
+            self.status_code = status_code
+
+        def raise_for_status(self):
+            if self.status_code >= 400:
+                raise Exception(self.status_code)
+
+        def json(self):
+            return json.loads(self.text)
+
+    if 'authorization' not in {k.lower() for k in kwargs['headers']}:
+        return MockResponse('<span>Not authorized</span>', 401)
+    if (kwargs['url'] or "").endswith('/api/v1/exec'):
+        return MockResponse(json.dumps(
+            {'uuid': 'foo', 'workflows': [{'uuid': 'uuid1'}]}
+        ), 200)
+    return MockResponse('<span>Not found</span>', 404)
+
+
+@mock.patch('sg.main.get_auth_headers')
+@mock.patch('requests.post', side_effect=mocked_requests_post)
+def test_main(mock_post, mock_get_auth_headers):
+    mock_get_auth_headers.return_value = {'Authorization': 'Bearer abcd'}
+    assert main.submit_sg_workload(
+        {'bucket': 'fake-bucket', 'name': 'something.bam'},
+        None
+    ) == ['uuid1']
+
+    mock_get_auth_headers.return_value = {}
+    with pytest.raises(Exception) as excinfo:
+        main.submit_sg_workload(
+            {'bucket': 'fake-bucket', 'name': 'something.bam'},
+            None
+        )
+    assert '401' in str(excinfo.value)
