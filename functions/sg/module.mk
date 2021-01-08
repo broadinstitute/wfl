af63a9b012ba45626c91f5e0a1ef96d82548e186
Jack Warren <jack@jackwarren.info>
2021-01-08 12:05:30 -0500

    [GH-1090] Automatic SG submission (#280)
    * A rough draft that would submit input files to WGS
    
    * Delete empty readme
    
    * Encode the fact that SG seems to only like unaligned BAMs
    
    * Reference name of pipeline correctly
    
    * Rename main
    
    * Add tests for the SG cloud function
    
    * fix formatting
    
    * Don't error on crams
    
    * Filter for specific paths
    
    * Clarify what "environment" was used for
    
    * Deployment script
    
    * deploy script fixes
    
    * add gotc-dev stub
    
    * simplify local vars
    
    * Correctly target my WFL
    
    * Fix ini to not have quotes
    
    * Make tests use right env var
    
    * Parse json properly
    
    * Print debug info better
    
    * Shorter line length
    
    * No need for gcs
    
    * Add a mocking test
    
    * Revert "No need for gcs"
    
    This reverts commit 2ab1190c
    
    * Remove gcs from the right place
    
    * Update entrypoint comment
    
    * Run tests in action
    
    * simplify filtering
    
    * Update deploy script for dev
    
    * Make Saman's suggestions

--- functions/sg/module.mk
+++ functions/sg/module.mk
@@ -1,3 +1,30 @@
 # Makefile for the functions/sg module
 
+REQUIRED_PYTHON_ENVIRONMENT := dev-requirements.txt
 include $(MAKE_INCLUDE_DIR)/modules.mk
+
+SRC_DIR  := $(MODULE_DIR)
+TEST_DIR := $(MODULE_DIR)/tests
+
+SCM_SRC := \
+	$(SRC_DIR)/__init__.py \
+	$(SRC_DIR)/main.py
+
+TEST_SCM_SRC = \
+	$(shell $(FIND) $(TEST_DIR) -type f -name '*.py') \
+	$(MODULE_DIR)/pytest.ini
+
+LOGFILE := $(DERIVED_MODULE_DIR)/unittest.log
+$(UNIT): $(SCM_SRC) $(TEST_SCM_SRC)
+	$(call using-python-environment, \
+		$(PYTHON) -m pytest $(TEST_DIR)/unit_tests.py | $(TEE) $(LOGFILE))
+	@$(TOUCH) $@
+
+$(LINT): $(SCM_SRC)
+	$(call using-python-environment, $(PYTHON) -m flake8 $(SCM_SRC))
+	@$(TOUCH) $@
+
+# Remove any python caches
+CLEAN_DIRS += \
+	$(shell $(FIND) $(MODULE_DIR) -type d -name '__pycache__') \
+	$(shell $(FIND) $(MODULE_DIR) -type d -name '.pytest_cache')
