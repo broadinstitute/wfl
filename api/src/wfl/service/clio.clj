d8f902077feff8b65ce230a466a6049bff2ff2c5
Tom Lyons <tbl3rd@gmail.com>
2021-01-13 18:41:55 -0500

    Check in a Rich Comment.

--- api/src/wfl/service/clio.clj
+++ api/src/wfl/service/clio.clj
@@ -2,6 +2,7 @@
   "Talk to Clio for some reason ..."
   (:require [clojure.java.io :as io]
             [clojure.data.json :as json]
+            [clojure.pprint :refer [pprint]]
             [clojure.string :as str]
             [clj-http.client :as http]
             [wfl.environments :as env]
@@ -15,6 +16,8 @@
   (-> "WFL_DEPLOY_ENVIRONMENT"
       (util/getenv "debug")
       wfl/error-or-environment-keyword env/stuff))
+
+(def deploy-environment (:gotc-dev env/stuff))
 
 (defn get-authorization-header
   "An Authorization header for talking to Clio where deployed."
@@ -32,10 +35,42 @@
 (defn post
   "Post THING to Clio server with metadata MD."
   [thing md]
-  (-> {:url (str/join "/" [(:clio deploy-environment) "api" "v1" "thing"])
+  (-> {:url (str/join "/" [(:clio deploy-environment) "api" "v1" thing])
        :method :post ;; :debug true :debug-body true
        :headers (merge {"Content-Type" "application/json"}
                        (get-authorization-header))
        :body (json/json-str md)}
       http/request :body
       (json/read-str :key-fn keyword)))
+
+(defn query-bam
+  "Return BAM entries with metadata MD."
+  [md]
+  (post (str/join "/" ["bam" "query"]) md))
+
+(defn add-bam
+  "Add a BAM entry with metadata MD."
+  [md]
+  (post (str/join "/" ["bam" "add"]) md))
+
+(comment
+  (with-open [out (io/writer (io/file "bam.edn"))]
+    (binding [*out* out]
+      (pprint (query-bam {}))))
+  (def sg
+    (filter :insert_size_metrics_path (query-bam {})))
+  {:workspace_name
+   "e7d5dbfb93d94563a11e6f563078a56a-TestWorkspace-9d7e4c079a1d456db35db758b9c3c57b",
+   :document_status "Normal",
+   :project "project7ad3f429aebd4e03b00679a70439135e",
+   :data_type "WGS",
+   :bam_path
+   "gs://broad-gotc-test-clio/bam/project7ad3f429aebd4e03b00679a70439135e/sample364a53a0da354e3d82c7bb3452991c40/v3/sample364a53a0da354e3d82c7bb3452991c40.bam",
+   :bam_md5 "1065398b26d944fc838a39ca78bd96e9",
+   :billing_project "broad-genomics-data",
+   :bai_path
+   "gs://broad-gotc-test-clio/bam/project7ad3f429aebd4e03b00679a70439135e/sample364a53a0da354e3d82c7bb3452991c40/v3/sample364a53a0da354e3d82c7bb3452991c40.bai",
+   :sample_alias "sample364a53a0da354e3d82c7bb3452991c40",
+   :version 3,
+   :location "GCP"}
+  )
