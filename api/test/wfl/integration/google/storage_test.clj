26cdb205f546c2571c94b2378f5dca24658bd485
Edmund Higham <edhigham@gmail.com>
2021-02-01 18:45:38 +0000

    split `gcs-test` into unit and integration. rename `storage-test`.

--- api/test/wfl/integration/google/storage_test.clj
+++ api/test/wfl/integration/google/storage_test.clj
@@ -1,4 +1,4 @@
-(ns wfl.unit.gcs-test
+(ns wfl.integration.google.storage-test
   "Test the Google Cloud Storage namespace."
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
@@ -16,31 +16,6 @@
 (def uid
   "A new unique string compressed from a random UUID."
   (str/replace (str (UUID/randomUUID)) "-" ""))
-
-(deftest gs-url-test
-  (testing "URL utilities"
-    (testing "parse-gs-url ok"
-      (is (= ["b" "obj/ect"]  (gcs/parse-gs-url "gs://b/obj/ect")))
-      (is (= ["b" "obj/ect/"] (gcs/parse-gs-url "gs://b/obj/ect/")))
-      (is (= ["b" ""]         (gcs/parse-gs-url "gs://b/")))
-      (is (= ["b" ""]         (gcs/parse-gs-url "gs://b"))))
-    (testing "gs-url ok"
-      (is (= "gs://b/ob/je/ct"  (gcs/gs-url "b" "ob/je/ct")))
-      (is (= "gs://b/ob/je/ct/" (gcs/gs-url "b" "ob/je/ct/")))
-      (is (= "gs://b/"          (gcs/gs-url "b" "")))
-      (is (= "gs://b/"          (gcs/gs-url "b" nil))))
-    (testing "parse-gs-url bad"
-      (is (thrown? IllegalArgumentException (gcs/parse-gs-url "")))
-      (is (thrown? IllegalArgumentException (gcs/parse-gs-url "x")))
-      (is (thrown? IllegalArgumentException (gcs/parse-gs-url "x/y")))
-      (is (thrown? IllegalArgumentException (gcs/parse-gs-url "/")))
-      (is (thrown? IllegalArgumentException (gcs/parse-gs-url "file://x/y")))
-      (is (thrown? IllegalArgumentException (gcs/parse-gs-url "gs:")))
-      (is (thrown? IllegalArgumentException (gcs/parse-gs-url "gs:/b/o")))
-      (is (thrown? IllegalArgumentException (gcs/parse-gs-url "gs:///o/"))))
-    (testing "gs-url bad"
-      (is (thrown? IllegalArgumentException (gcs/gs-url ""  "")))
-      (is (thrown? IllegalArgumentException (gcs/gs-url ""  "o"))))))
 
 (def local-file-name
   "A disposable local file name for object-test."
