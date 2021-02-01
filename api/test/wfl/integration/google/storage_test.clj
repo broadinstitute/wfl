2dc5c305f2eaa3628e398a1d348b542347a2cf13
Edmund Higham <edhigham@gmail.com>
2021-02-01 20:23:27 +0000

    why?

--- api/test/wfl/integration/google/storage_test.clj
+++ api/test/wfl/integration/google/storage_test.clj
@@ -13,43 +13,32 @@
   "Test in this Google Cloud project."
   "broad-gotc-dev-storage")
 
-(def uid
-  "A new unique string compressed from a random UUID."
-  (str/replace (str (UUID/randomUUID)) "-" ""))
-
-(def local-file-name
-  "A disposable local file name for object-test."
-  (str/join "-" ["wfl" "test" uid]))
-
-(defn cleanup-object-test
-  "Clean up after the object-test."
-  []
-  (io/delete-file local-file-name :silently))
-
 (deftest object-test
-  (try
-    (with-temporary-cloud-storage-folder fixtures/gcs-test-bucket
-      (fn [url]
-        (testing "Objects"
-          (let [[bucket src-folder] (gcs/parse-gs-url url)
-                dest-folder (str src-folder "destination/")
-                object {:name (str src-folder "test")
-                        :contentType "text/plain"}
-                dockerfile "Dockerfile"]
-            (testing "upload"
-              (let [result (gcs/upload-file dockerfile bucket (:name object))]
-                (is (= object (select-keys result (keys object))))
-                (is (= bucket (:bucket result)))))
-            (testing "list"
-              (let [result (gcs/list-objects bucket src-folder)]
-                (is (= 1 (count result)))
-                (is (= object (select-keys (first result) (keys object))))))
-            (testing "copy"
-              (is (gcs/copy-object bucket (str src-folder "test") bucket (str dest-folder "test"))))
-            (testing "download"
+  (with-temporary-cloud-storage-folder fixtures/gcs-test-bucket
+    (fn [url]
+      (testing "Objects"
+        (let [[bucket src-folder] (gcs/parse-gs-url url)
+              dest-folder (str src-folder "destination/")
+              object {:name (str src-folder "test")
+                      :contentType "text/plain"}
+              dockerfile "Dockerfile"]
+          (testing "upload"
+            (let [result (gcs/upload-file dockerfile bucket (:name object))]
+              (is (= object (select-keys result (keys object))))
+              (is (= bucket (:bucket result)))))
+          (testing "list"
+            (let [result (gcs/list-objects bucket src-folder)]
+              (is (= 1 (count result)))
+              (is (= object (select-keys (first result) (keys object))))))
+          (testing "copy"
+            (is (gcs/copy-object bucket (str src-folder "test") bucket (str dest-folder "test"))))
+          (testing "download"
+            (let [local-file-name (str/join "-" ["wfl" "test" (UUID/randomUUID)])]
               (gcs/download-file local-file-name bucket (str dest-folder "test"))
-              (is (= (slurp dockerfile) (slurp local-file-name))))))))
-    (finally (cleanup-object-test))))
+              (try
+                (is (= (slurp dockerfile) (slurp local-file-name)))
+                (finally
+                  (io/delete-file local-file-name :silently))))))))))
 
 (deftest userinfo-test
   (testing "no \"Authorization\" header in request should throw"
