45beea49611091c90eda814fd2d30372aacbe93a
Tom Lyons <tbl3rd@gmail.com>
2021-02-03 09:53:45 -0500

    Use the latest Warp pipeline.

--- api/src/wfl/module/sg.clj
+++ api/src/wfl/module/sg.clj
@@ -24,7 +24,7 @@
 
 (def workflow-wdl
   "The top-level WDL file and its version."
-  {:release "b0e3cfef18fc3c4126b7b835ab2b253599a18904"
+  {:release "948bfc0f4251c349adce4d6a6475b2bb31bbad22"
    :path    (str "beta-pipelines/broad/somatic/single_sample/wgs/"
                  "gdc_genome/GDCWholeGenomeSomaticSingleSample.wdl")})
 
