6b10cb5021c80b983488780d36120b109ecd5cef
Rex <rexwangcc@gmail.com>
2021-02-04 11:01:24 -0500

    Commit initial code using java client.

---
+++ api/src/wfl/service/bigquery.clj
@@ -0,0 +1,43 @@
+(ns wfl.service.bigquery
+  "Interact with GCloud Biqquery service."
+  (:require [clojure.data.json :as json]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clj-http.client :as http]
+            [wfl.environments :as env]
+            [wfl.util         :as util]
+            [wfl.wfl         :as wfl]
+            [wfl.once :as once])
+  (:import [com.google.cloud.bigquery BigQuery
+                                      BigQueryOptions
+                                      JobId
+                                      JobInfo
+                                      QueryJobConfiguration
+                                      BigQuery$DatasetListOption]
+           [java.util ArrayList Collections Random UUID]))
+
+(defn service
+  "Build a BigQuery service instance from Google Cloud PROJECT-ID."
+  [project-id]
+  (let [credentials (once/service-account-credentials)]
+    (-> (BigQueryOptions/newBuilder)
+      (.setCredentials credentials)
+      (.setProjectId project-id)
+      .build .getService)))
+
+(comment
+  (service "broad-jade-dev-data"))
+
+(defn list-datasets
+  "Return a list of all datasets under Google Cloud PROJECT-ID."
+  [project-id]
+  (letfn [(conj-dataset [coll dataset]
+            (conj coll (.getDataset (.getDatasetId dataset))))]
+    (let [service          (service project-id)
+          dataset-iterator (-> service
+                             (.listDatasets (into-array BigQuery$DatasetListOption []))
+                             .iterateAll)]
+      (reduce conj-dataset [] dataset-iterator))))
+
+(comment
+  (list-datasets "broad-jade-dev-data"))
