c403a6752eb60685bb6a979ae8afaf5b7b7e015f
Rex <rexwangcc@gmail.com>
2021-02-16 15:21:24 -0500

    Fix issues generated from refactoring.

--- api/src/wfl/api/routes.clj
+++ api/src/wfl/api/routes.clj
@@ -45,8 +45,8 @@
            :handler   (fn [_] (handlers/succeed {:oauth2-client-id (env/getenv "WFL_OAUTH2_CLIENT_ID")}))
            :responses {200 {:body {:oauth2-client-id string?}}}
            :swagger   {:tags ["Informational"]}}}]
-   ["/api/v1/wfl.environment"
-    {:get  {:summary "Get all of the wfl.environment the server knows"
+   ["/api/v1/environment"
+    {:get  {:summary "Get all of the environment the server knows"
             :parameters nil
             :responses {200 {:body map?}}
             :handler (handlers/success env/stuff)}}]
