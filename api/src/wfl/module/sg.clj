a3cbdf433050d6ebe06d696bc7e83b08c08f95cf
Tom Lyons <tbl3rd@gmail.com>
2021-02-19 14:04:01 -0500

    Need cromwell now, so move hacks to sg.

--- api/src/wfl/module/sg.clj
+++ api/src/wfl/module/sg.clj
@@ -211,3 +211,69 @@
 (defoverload workloads/update-workload!   pipeline update-sg-workload!)
 (defoverload workloads/load-workload-impl pipeline
   batch/load-batch-workload-impl)
+
+;; Hacks follow.  Blame tbl.
+
+(defn sample->clio-cram
+  "Clio CRAM metadata for SAMPLE."
+  [sample]
+  (let [translation {:location     "Processing Location"
+                     :project      "Project"
+                     :sample_alias "Collaborator Sample ID"
+                     :version      "Version"}]
+    (letfn [(translate [m [k v]] (assoc m k (sample v)))]
+      (reduce translate {:data_type "WGS"} translation))))
+
+(defn tsv->crams
+  "Translate TSV file to CRAM records from `clio`."
+  [clio tsv]
+  (map (comp (partial clio/query-cram clio) sample->clio-cram)
+       (util/map-tsv-file tsv)))
+
+(defn cram->inputs
+  "Translate Clio `cram` record to SG workflow inputs."
+  [cram]
+  (let [translation {:base_file_name :sample_alias
+                     :input_cram     :cram_path}
+        contam "gs://gatk-best-practices/somatic-hg38/small_exac_common_3.hg38.vcf.gz"
+        fasta  "gs://gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.fasta"
+        dbsnp  "gs://broad-gotc-dev-storage/temp_references/gdc/dbsnp_144.hg38.vcf.gz"
+        bogus  {:contamination_vcf       contam
+                :contamination_vcf_index (str contam ".tbi")
+                :cram_ref_fasta          fasta
+                :cram_ref_fasta_index    (str fasta ".fai")
+                :dbsnp_vcf               dbsnp
+                :dbsnp_vcf_index         (str dbsnp ".tbi")}]
+    (letfn [(translate [m [k v]] (assoc m k (v cram)))]
+      {:inputs (reduce translate bogus translation)})))
+
+(defn crams->workload
+  "Return a workload request to process `crams` with SG pipeline"
+  [crams]
+  {:executor "https://cromwell-gotc-auth.gotc-prod.broadinstitute.org"
+   :output   "gs://broad-prod-somatic-genomes-output"
+   :pipeline "GDCWholeGenomeSomaticSingleSample"
+   :project  "(Test) tbl/GH-1196-sg-prod-data"
+   :items    (mapv cram->inputs crams)})
+
+(comment
+  (do
+    (def dev  "https://clio.gotc-dev.broadinstitute.org")
+    (def prod "https://clio.gotc-prod.broadinstitute.org")
+    (def tsv
+      "../NCI_EOMI_Ship1_WGS_SeqComplete_94samples_forGDCPipelineTesting.tsv")
+    (def crams (tsv->crams prod tsv))
+    (def cram (first crams)))
+  (util/map-tsv-file tsv)
+  (count crams)
+  (query-cram dev cram)
+  (query-cram prod cram)
+  crams
+  (crams->workload crams)
+  (-> "./clio-cram-records.edn"
+      clojure.java.io/file
+      clojure.java.io/writer
+      (->> (clojure.pprint/pprint crams)))
+  cram
+  )
+
