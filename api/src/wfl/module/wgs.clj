d75a5cca58ab2daaf885b943cf64382aee7a2902
Rex <rexwangcc@gmail.com>
2021-02-23 09:49:10 -0500

    Use WGS wdl on develop branch.

--- api/src/wfl/module/wgs.clj
+++ api/src/wfl/module/wgs.clj
@@ -16,7 +16,7 @@
 
 (def workflow-wdl
   "The top-level WDL file and its version."
-  {:release "ExternalWholeGenomeReprocessing_v1.1.1"
+  {:release "develop"
    :path    "pipelines/broad/reprocessing/external/wgs/ExternalWholeGenomeReprocessing.wdl"})
 
 (def ^:private cromwell-label
