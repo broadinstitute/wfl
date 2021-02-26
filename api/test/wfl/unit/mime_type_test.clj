e8d977bd359c8d66c4eef1e5c1e21629fc1631ae
Edmund Higham <ehigham@users.noreply.github.com>
2021-02-26 21:15:19 +0000

    [GH-1172] Add `inputs.edn` for `sarscov2_illumina_full` (#332)
    * organise workflow edn into subdirectories
    
    * dont run unnecessary recipes
    
    * tidy-up bucket
    
    * tidy-up bucket for sarscov2 + name consistently
    
    * add sarscov2_illumina_full/inputs.edn
    
    * fix terra workspace in sarscov2_illumina_full/inputs.edn
    
    * i has the dumb
    
    * moar dumb

--- api/test/wfl/unit/mime_type_test.clj
+++ api/test/wfl/unit/mime_type_test.clj
@@ -19,11 +19,11 @@
           (str filename " does not have the expected mime-type")))))
 
 (deftest test-workflow-mime-types
-  (let [exclude? #{"gs://broad-gotc-dev-wfl-ptc-test-inputs/covid-19/sarscov2_illumina_full/call-demux_deplete/demux_deplete/955c74b3-854a-4075-839f-856d0f41e020/call-sra_meta_prep/write_lines_bc5302b8fdd987961b17ced77e1da4ab.tmp"}
-        cases [[(workflows/read-resource "assemble-refbased-outputs")
-                (-> "assemble-refbased-description" workflows/read-resource :outputs)]
-               [(workflows/read-resource "sarscov2-illumina-full-outputs")
-                (-> "sarscov2-illumina-full-description" workflows/read-resource :outputs)]]]
+  (let [exclude? #{"gs://broad-gotc-dev-wfl-ptc-test-inputs/sarscov2_illumina_full/outputs/call-demux_deplete/demux_deplete/955c74b3-854a-4075-839f-856d0f41e020/call-sra_meta_prep/write_lines_bc5302b8fdd987961b17ced77e1da4ab.tmp"}
+        cases [[(workflows/read-resource "assemble_refbased/outputs")
+                (-> "assemble_refbased/description" workflows/read-resource :outputs)]
+               [(workflows/read-resource "sarscov2_illumina_full/outputs")
+                (-> "sarscov2_illumina_full/description" workflows/read-resource :outputs)]]]
     (doseq [[values description] cases]
       (let [type (workflows/make-object-type description)]
         (doseq [filename (workflows/get-files type values)]
