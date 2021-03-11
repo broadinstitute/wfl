b6eca6afe06ffba925fe0db18f3864d90414d49b
Edmund Higham <edhigham@gmail.com>
2021-03-11 20:32:32 +0000

    unit test `traverse` and `foldl`

--- api/test/wfl/tools/workflows.clj
+++ api/test/wfl/tools/workflows.clj
@@ -8,60 +8,64 @@
        (map #(set/rename-keys % {:name :fieldName :valueType :fieldType}))
        (assoc {:typeName "Object"} :objectFieldTypes)))
 
+(defn ^:private make-type-environment [{:keys [objectFieldTypes] :as _object}]
+  (let [name->type (into {}
+                         (for [{:keys [fieldName fieldType]} objectFieldTypes]
+                           [(keyword fieldName) fieldType]))]
+    (fn [varname]
+      (or (name->type varname)
+          (throw (ex-info "No type definition found for name."
+                          {:name        (when varname (name varname))
+                           :environment name->type}))))))
+
 (defn traverse
   "Use the workflow `type` to traverse the `object`, calling `f` on the values
    with primitive types."
   [f type object]
-  (letfn [(make-type-environment [{:keys [objectFieldTypes]}]
-            (into {} (for [{:keys [fieldName fieldType]} objectFieldTypes]
-                       [(keyword fieldName) fieldType])))]
-    ((fn go [type value]
-       (case (:typeName type)
-         "Array"
-         (let [array-type (:arrayType type)]
-           (map #(go array-type %) value))
-         "Map"
-         (let [{:keys [keyType valueType]} (:mapType type)]
-           (into {} (map (fn [[k v]] [(go keyType k) (go valueType v)]) value)))
-         "Object"
-         (let [name->type (make-type-environment type)]
-           (into {} (map (fn [[k v]] [k (go (name->type k) v)]) value)))
-         "Optional"
-         (when value (go (:optionalType type) value))
-         "Pair"
-         (let [ts (mapv (:pairType type) [:leftType :rightType])]
-           (map go ts value))
-         (f (:typeName type) value)))
-     type object)))
+  ((fn go [type value]
+     (case (:typeName type)
+       "Array"
+       (let [array-type (:arrayType type)]
+         (map #(go array-type %) value))
+       "Map"
+       (let [{:keys [keyType valueType]} (:mapType type)]
+         (into {} (map (fn [[k v]] [(go keyType k) (go valueType v)]) value)))
+       "Object"
+       (let [name->type (make-type-environment type)]
+         (into {} (map (fn [[k v]] [k (go (name->type k) v)]) value)))
+       "Optional"
+       (when value (go (:optionalType type) value))
+       "Pair"
+       (let [ts (mapv (:pairType type) [:leftType :rightType])]
+         (map go ts value))
+       (f (:typeName type) value)))
+   type object))
 
 (defn foldl
   "Use the workflow `type` to left-fold the `object`, calling `f` on the values
    with primitive types with the current state."
   [f init type object]
-  (letfn [(make-type-environment [{:keys [objectFieldTypes]}]
-            (into {} (for [{:keys [fieldName fieldType]} objectFieldTypes]
-                       [(keyword fieldName) fieldType])))]
-    ((fn go [state type value]
-       (case (:typeName type)
-         "Array"
-         (let [array-type (:arrayType type)]
-           (reduce #(go %1 array-type %2) state value))
-         "Map"
-         (let [{:keys [keyType valueType]} (:mapType type)]
-           (reduce-kv #(-> (go %1 keyType %2) (go valueType %3)) state value))
-         "Object"
-         (let [name->type (make-type-environment type)]
-           (reduce-kv #(go %1 (name->type %2) %3) state value))
-         "Optional"
-         (if value (go state (:optionalType type) value) state)
-         "Pair"
-         (let [{:keys [leftType rightType]} (:pairType type)]
-           (-> (go state leftType  (first value))
-               (go       rightType (second value))))
-         (f state (:typeName type) value)))
-     init type object)))
+  ((fn go [state type value]
+     (case (:typeName type)
+       "Array"
+       (let [array-type (:arrayType type)]
+         (reduce #(go %1 array-type %2) state value))
+       "Map"
+       (let [{:keys [keyType valueType]} (:mapType type)]
+         (reduce-kv #(-> (go %1 keyType %2) (go valueType %3)) state value))
+       "Object"
+       (let [name->type (make-type-environment type)]
+         (reduce-kv #(go %1 (name->type %2) %3) state value))
+       "Optional"
+       (if value (go state (:optionalType type) value) state)
+       "Pair"
+       (let [{:keys [leftType rightType]} (:pairType type)]
+         (-> (go state leftType  (first value))
+             (go       rightType (second value))))
+       (f state (:typeName type) value)))
+   init type object))
 
 (defn get-files [type value]
   "Return the unique set of objects in `value` of WDL type `File`."
-  (letfn [(f [files type object] (if (= "File" type) (cons object files) files))]
-    (set (foldl f [] type value))))
+  (letfn [(f [files type object] (if (= "File" type) (conj files object) files))]
+    (foldl f #{} type value)))
---
+++ api/test/wfl/unit/workflows_test.clj
@@ -0,0 +1,105 @@
+(ns wfl.unit.workflows-test
+  (:require [clojure.test :refer :all]
+            [wfl.tools.workflows :as workflows]
+            [wfl.tools.resources :as resources]
+            [wfl.util :as util])
+  (:import (clojure.lang ExceptionInfo)))
+
+(defn ^:private filtering-type [test? type object]
+  (letfn [(f [vs t v] (if (test? t) (conj vs v) vs))]
+    (workflows/foldl f #{} type object)))
+
+(defn ^:private get-primitives [type object]
+  (letfn [(f [ts t _] (conj ts t))]
+    (workflows/foldl f #{} type object)))
+
+(defn ^:private lift-value [f] (fn [_ value] (f value)))
+(def ^:private  vectorize (partial workflows/traverse (lift-value vector)))
+
+(defn ^:private make-output-type [resource-file]
+  (-> resource-file
+      resources/read-resource
+      :outputs
+      workflows/make-object-type))
+
+(deftest test-no-such-name
+  (let [type {:typeName "Object" :objectFieldTypes []}]
+    (testing "foldl"
+      (is (thrown-with-msg?
+           ExceptionInfo
+           #"No type definition found for name\."
+           (workflows/foldl (constantly nil) nil type {:no-such-name nil}))))
+    (testing "traverse"
+      (is (thrown-with-msg?
+           ExceptionInfo
+           #"No type definition found for name\."
+           (workflows/traverse (constantly nil) type {:no-such-name nil}))))))
+
+(deftest test-primitive-types
+  (let [type (make-output-type "primitive.edn")
+        outputs {:outbool   true
+                 :outfile   "lolcats.txt"
+                 :outfloat  3.14
+                 :outint    3
+                 :outstring "Hello, World!"}]
+    (testing "foldl"
+      (is (= #{"Boolean" "File" "Float" "Int" "String"}
+             (get-primitives type outputs)))
+      (is (= #{"Boolean" "File" "Float" "Int"}
+             (get-primitives type (dissoc outputs :outstring))))
+      (is (= #{"lolcats.txt"} (workflows/get-files type outputs))))
+    (testing "traverse"
+      (is (= (util/map-vals vector outputs) (vectorize type outputs))))))
+
+(deftest test-array-types
+  (let [type    (make-output-type "compound.edn")
+        outputs {:outarray  ["clojure" "is" "fun"]}]
+    (testing "foldl"
+      (is (= 3 (count (filtering-type #(= % "String") type outputs)))))
+    (testing "traverse"
+      (is (= (util/map-vals #(map vector %) outputs) (vectorize type outputs))))))
+
+(deftest test-map-types
+  (let [type    (make-output-type "compound.edn")
+        outputs {:outmap {"foo" "lolcats.txt" "bar" "in.gif"}}]
+    (testing "foldl"
+      (is (= #{"foo" "bar"} (filtering-type #(= % "String") type outputs)))
+      (is (= #{"lolcats.txt" "in.gif"} (filtering-type #(= % "File") type outputs))))
+    (testing "traverse"
+      (is (= {:outmap {["foo"] ["lolcats.txt"] ["bar"] ["in.gif"]}}
+             (vectorize type outputs))))))
+
+(deftest test-optional-types
+  (let [type    (make-output-type "compound.edn")
+        without {:outoptional  nil}
+        with    {:outoptional  "lolcats.txt"}]
+    (testing "foldl"
+      (is (= 0 (count (workflows/get-files type without))))
+      (is (= 1 (count (workflows/get-files type with)))))
+    (testing "traverse"
+      (is (= without (vectorize type without)))
+      (is (= (util/map-vals vector with) (vectorize type with))))))
+
+(deftest test-pair-types
+  (let [type    (make-output-type "compound.edn")
+        outputs {:outpair [3 3.14]}]
+    (testing "foldl"
+      (is (= #{3.14} (filtering-type #(= % "Float") type outputs))))
+    (testing "traverse"
+      (is (= {:outpair [9 3.14]}
+             (workflows/traverse
+              (fn [type v] (if (= type "Int") (* v v) v))
+              type
+              outputs))))))
+
+(deftest test-struct-types
+  (let [type    (make-output-type "compound.edn")
+        outputs {:outstruct {:value 3}}]
+    (testing "foldl"
+      (is (= #{3} (filtering-type #(= % "Int") type outputs))))
+    (testing "traverse"
+      (is (= {:outstruct {:value 9}}
+             (workflows/traverse
+              (fn [type v] (if (= type "Int") (* v v) v))
+              type
+              outputs))))))
