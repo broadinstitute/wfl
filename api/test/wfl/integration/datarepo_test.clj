e9a5ead5eca73acca62dcb92fb98b824c0dd888d
Edmund Higham <edhigham@gmail.com>
2021-03-11 20:36:20 +0000

    make the TDR integ test even faster

--- api/test/wfl/integration/datarepo_test.clj
+++ api/test/wfl/integration/datarepo_test.clj
@@ -34,65 +34,43 @@
 
 (def ^:private pi (* 4 (Math/atan 1)))
 
-(def ^:private primitive-outputs
+(def ^:private outputs
   {:outbool   true
    :outfile   "gs://broad-gotc-dev-wfl-ptc-test-inputs/external-reprocessing/exome/develop/not-a-real.unmapped.bam"
    :outfloat  pi
    :outint    27
    :outstring "Hello, World!"})
 
-(def ^:private from-primitive-outputs
+(def ^:private from-outputs
   {:boolean "outbool"
    :fileref "outfile"
    :float   "outfloat"
    :integer "outint"
    :string  "outstring"})
 
-(def ^:private compound-outputs
-  {:outarray    ["foo" "bar" "baz"]
-   :outmap      {"bam" "gs://broad-gotc-dev-wfl-ptc-test-inputs/external-reprocessing/exome/develop/not-a-real.unmapped.bam"}
-   :outoptional nil
-   :outpair     [3, pi]
-   :outstruct   {:value 5}})
-
-(def ^:private from-compound-outputs
-  {:strings "outarray"
-   :floats  "inpair"
-   :fileref "outoptional"
-   :string  {:struct "outstruct" :map "outmap"}})
-
 (deftest test-ingest-pipeline-outputs
   (let [dataset-json "testing-dataset.json"
         table-name   "parameters"
-        tdr-profile  (env/getenv "WFL_TDR_DEFAULT_PROFILE")]
+        tdr-profile  (env/getenv "WFL_TDR_DEFAULT_PROFILE")
+        outputs-type (-> (resources/read-resource "primitive.edn")
+                         :outputs
+                         workflows/make-object-type)
+        workflow-id  (UUID/randomUUID)]
     (fixtures/with-fixtures
       [(fixtures/with-temporary-cloud-storage-folder
          "broad-gotc-dev-wfl-ptc-test-inputs")
        (fixtures/with-temporary-dataset
          (datasets/unique-dataset-request tdr-profile dataset-json))]
       (fn [[temp dataset]]
-        (doseq [[outputs from-outputs outputs-type workflow-id]
-                [[primitive-outputs
-                  from-primitive-outputs
-                  (-> (resources/read-resource "primitive.edn")
-                      :outputs
-                      workflows/make-object-type)
-                  (UUID/randomUUID)]
-                 [compound-outputs
-                  from-compound-outputs
-                  (-> (resources/read-resource "compound.edn")
-                      :outputs
-                      workflows/make-object-type)
-                  (UUID/randomUUID)]]]
-          (let [table-url (str temp workflow-id "/table.json")]
-            (-> (->> (workflows/get-files outputs-type outputs)
-                     (datasets/ingest-files tdr-profile dataset workflow-id))
-                (replace-urls-with-file-ids outputs-type outputs)
-                (datasets/rename-gather from-outputs)
-                (json/write-str :escape-slash false)
-                (gcs/upload-content table-url))
-            (let [{:keys [bad_row_count row_count]}
-                  (datarepo/poll-job
-                   (datarepo/ingest-table dataset table-url table-name))]
-              (is (= 1 row_count))
-              (is (= 0 bad_row_count)))))))))
+        (let [table-url (str temp workflow-id "/table.json")]
+          (-> (->> (workflows/get-files outputs-type outputs)
+                   (datasets/ingest-files tdr-profile dataset workflow-id))
+              (replace-urls-with-file-ids outputs-type outputs)
+              (datasets/rename-gather from-outputs)
+              (json/write-str :escape-slash false)
+              (gcs/upload-content table-url))
+          (let [{:keys [bad_row_count row_count]}
+                (datarepo/poll-job
+                 (datarepo/ingest-table dataset table-url table-name))]
+            (is (= 1 row_count))
+            (is (= 0 bad_row_count))))))))
