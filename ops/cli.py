adac9d5ab0a6872dbabd60697b6400b65b476b67
Rhian Anthony <rhian.anthony@gmail.com>
2021-03-15 11:50:32 -0400

    Changing cli.py to reflect deployment from helmfile rather than helm (#344)
    * Changing cli.py to reflect deployment from helmfile rather than helm
    
    * removing deploy mapping from cli.py since it is redundant with deploy-from-tag
    
    Co-authored-by: rhiananthony <ranthony@broadinstitute.org>

--- ops/cli.py
+++ ops/cli.py
@@ -230,12 +230,11 @@ def publish_docker_images(config: WflInstanceConfig) -> None:
     success("Published Docker images")
 
 
-def helm_deploy_wfl(config: WflInstanceConfig) -> None:
+def helmfile_deploy_wfl(config: WflInstanceConfig) -> None:
     """Deploy the pushed docker images for the stored version to the stored cluster."""
     info(f"=>  Deploying to {config.cluster_name} in {config.cluster_namespace} namespace")
-    info("    This must run on a non-split VPN (or on a specifically-allowed Jenkins agent)", plain=True)
-    shell(f"helm upgrade {config.instance_id}-wfl gotc-charts/wfl -f {config.rendered_values_file} --install "
-          f"--namespace {config.cluster_namespace}")
+    info("    This must run on a non-split VPN", plain=True)
+    shell(f"helmfile --selector name={config.instance_id}-wfl sync")
     success("WFL deployed")
 
 
@@ -348,26 +347,6 @@ command_mapping: Dict[str, List[Callable[[WflInstanceConfig], None]]] = {
         configure_cloud_sql_proxy,
         print_cloud_sql_proxy_instructions
     ],
-    "deploy": [
-        check_env_instance_present,
-        read_version,
-        infer_missing_arguments_pre_validate,
-        validate_cloud_sql_name,
-        validate_cluster_name,
-        infer_missing_arguments,
-        configure_kubectl,
-        configure_helm,
-        render_values_file,
-        print_config,
-        exit_if_dry_run,
-        prompt_deploy_version,
-        configure_cloud_sql_proxy,
-        publish_docker_images,
-        helm_deploy_wfl,
-        run_liquibase_migration,
-        stop_cloud_sql_proxy,
-        print_deployment_success
-    ],
     "deploy-from-tag": [
         check_env_instance_present,
         read_version,
@@ -377,12 +356,10 @@ command_mapping: Dict[str, List[Callable[[WflInstanceConfig], None]]] = {
         infer_missing_arguments,
         check_git_tag,
         configure_kubectl,
-        configure_helm,
-        render_values_file,
         print_config,
         exit_if_dry_run,
         configure_cloud_sql_proxy,
-        helm_deploy_wfl,
+        helmfile_deploy_wfl,
         run_liquibase_migration,
         stop_cloud_sql_proxy,
         print_deployment_success
