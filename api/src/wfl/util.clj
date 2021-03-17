89aa25221f8808117453e579a1a249bb5bd13437
Rex <rexwangcc@gmail.com>
2021-03-17 17:39:12 -0400

    Fix time.

--- api/src/wfl/util.clj
+++ api/src/wfl/util.clj
@@ -7,9 +7,7 @@
             [clojure.tools.logging :as log]
             [wfl.wfl :as wfl])
   (:import [java.io File Writer IOException]
-           [java.text SimpleDateFormat]
-           [java.time OffsetDateTime Instant ZoneOffset ZoneId Clock]
-           [java.time.format DateTimeFormatter]
+           [java.time OffsetDateTime Clock LocalDate]
            [java.time.temporal ChronoUnit]
            [java.nio.file Files]
            [java.nio.file.attribute FileAttribute]
@@ -380,25 +378,13 @@
   (letfn [(make-part [[k v]] {:name (name k) :content v})]
     (map make-part parts)))
 
-(defn datetime->str
-  "Parse `datetime` object into string, with optional `format`.
-
-   Note this function uses UTC as the time zone offset by default."
-  ([^OffsetDateTime datetime format]
-   (let [formatter (-> format
-                       DateTimeFormatter/ofPattern)]
-     (.format formatter datetime)))
-  ([^OffsetDateTime datetime]
-   (datetime->str datetime "yyyy-MM-dd")))
-
-;; see https://github.com/broadinstitute/wfl/pull/338#discussion_r592618718
-(defn now
-  "Return now in ^OffsetDateTime."
+(defn today
+  "Return a ^LocalDate of today's date in UTC."
   []
-  (OffsetDateTime/now))
+  (LocalDate/now (Clock/systemUTC)))
 
-(defn days-from-now
-  "Return n-days from now in ^OffsetDateTime, either
+(defn days-from-today
+  "Return n-days from today's date in ^LocalDate, either
    backward or forward depends on the sign of n."
   [^Integer n]
-  (.plus (now) n ChronoUnit/DAYS))
+  (.plus (today) n ChronoUnit/DAYS))
