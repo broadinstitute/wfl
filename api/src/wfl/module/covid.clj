a94500cc28c982fa7c466c4fabf314b15e876e0a
Chengchen(Rex) Wang <14366016+rexwangcc@users.noreply.github.com>
2021-04-16 08:47:23 -0400

    Create an interface all COVID work can build on top of. (#369)
    * Create an interface all hornets work on top of.
    
    * Apply suggestions from code review
    
    * Update readme.md
    
    Build Boad.

---
+++ api/src/wfl/module/covid.clj
@@ -0,0 +1,41 @@
+(ns wfl.module.covid
+  "Handle COVID processing."
+  (:require [clojure.data.json :as json]
+            [clojure.set :as set]
+            [clojure.string :as str]
+            [clojure.tools.logging.readable :as log]
+            [wfl.api.workloads :as workloads :refer [defoverload]]
+            [wfl.jdbc :as jdbc]
+            [wfl.module.batch :as batch]
+            [wfl.service.datarepo :as datarepo]
+            [wfl.service.firecloud :as firecloud]
+            [wfl.service.google.storage :as gcs]
+            [wfl.service.postgres :as postgres]
+            [wfl.util :as util]
+            [wfl.wfl :as wfl]))
+
+(def pipeline "Sarscov2IlluminaFull")
+
+;; TODO: implement COVID workload creation
+;;  - make sure permissions/inputs are right upfront
+;;  - dispatch on source/sink/executor
+;;  - store information into database
+(defn create-covid-workload!
+  [tx {:keys [source sink executor] :as request}])
+
+;; TODO: implement progressive (private) functions inside this update loop
+(defn update-covid-workload!
+  "Use transaction `tx` to batch-update `workload` statuses."
+  [tx {:keys [started finished] :as workload}]
+  (letfn [(update! [{:keys [id] :as workload}]
+            (postgres/batch-update-workflow-statuses! tx workload)
+            (postgres/update-workload-status! tx workload)
+            (workloads/load-workload-for-id tx id))]
+    (if (and started (not finished)) (update! workload) workload)))
+
+(defoverload workloads/create-workload!   pipeline create-covid-workload!)
+(defoverload workloads/start-workload!    pipeline start-covid-workload!)
+(defoverload workloads/update-workload!   pipeline update-covid-workload!)
+(defoverload workloads/stop-workload!     pipeline batch/stop-workload!)
+(defoverload workloads/load-workload-impl pipeline
+  batch/load-batch-workload-impl)
