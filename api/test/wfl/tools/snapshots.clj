ce5c7c63dafc7d82baaefb1633012192e9c3fda0
Rex <rexwangcc@gmail.com>
2021-04-19 23:23:16 -0400

    Remove outdated test code.

--- api/test/wfl/tools/snapshots.clj
+++ api/test/wfl/tools/snapshots.clj
@@ -12,21 +12,3 @@
     (-> (datarepo/make-snapshot-request dataset columns table row-ids)
         (update :name util/randomize)
         (update :profileId (constantly tdr-profile)))))
-
-;; Partition row IDs into batches of 500 to keep TDR happy.
-;; Ask Ruchi for the reference to the bug ticket if there's one.
-(defn create-snapshots
-  "Return snapshot requests of up to 500 row ID
-   from `table` in `dataset` for `tdr-profile`.
-
-   Note the dataset row query time range is set to (yesterday, today]
-   for testing purposes for now."
-  [tdr-profile dataset table]
-  (let [today                (util/today)
-        yesterday            (util/days-from-today -1)
-        unique-request-batch (partial unique-snapshot-request tdr-profile dataset table)]
-    (->> (datarepo/query-table-between dataset table [yesterday today] [:datarepo_row_id])
-         flatten
-         (partition-all 500)
-         (map unique-request-batch)
-         (map (partial unique-snapshot-request tdr-profile dataset table)))))
