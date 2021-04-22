4f8e23ec6774714dbf0dc1c2fb3befc48731a44c
Rex <rexwangcc@gmail.com>
2021-04-22 17:39:12 -0400

    Commit updated implementation (WIP).

--- api/src/wfl/module/covid.clj
+++ api/src/wfl/module/covid.clj
@@ -16,12 +16,36 @@
 
 (def pipeline "Sarscov2IlluminaFull")
 
+(defn ^:private add-workload-table!
+  "Use transaction `tx` to add a `CromwellWorkflow` table
+  for a `workload-request` running `workflow-wdl`."
+  [tx workload-request]
+  (let [{:keys [pipeline]} workload-request
+        create "CREATE TABLE %s OF CromwellWorkflow (PRIMARY KEY (id))"
+        setter "UPDATE workload SET pipeline = ?::pipeline WHERE id = ?"
+        [{:keys [id]}]
+        (-> workload-request
+          ;; FIXME: update the DB schema to write these new information in
+            (select-keys [:creator :executor :source :sink :project :labels :watchers])
+
+            (merge (select-keys (wfl/get-the-version) [:commit :version]))
+          ;; FIXME: how to write :release and :wdl
+            (assoc :release release :wdl path :uuid (UUID/randomUUID))
+            (->> (jdbc/insert! tx :workload)))
+        table (format "%s_%09d" pipeline id)]
+    (jdbc/execute!       tx [setter pipeline id])
+    (jdbc/db-do-commands tx [(format create table)])
+    (jdbc/update!        tx :workload {:items table} ["id = ?" id])
+    [id table]))
+
 ;; TODO: implement COVID workload creation
-;;  - make sure permissions/inputs are right upfront
-;;  - dispatch on source/sink/executor
-;;  - store information into database
 (defn create-covid-workload!
-  [tx {:keys [source sink executor] :as request}])
+  [tx {:keys [source sink executor] :as request}]
+  ;; TODO: validation
+  ;; TODO: dispatch on source/sink/executor
+  (-> request
+      (add-workload-table! tx)
+      (workloads/load-workload-for-id tx id)))
 
 ;; TODO: implement progressive (private) functions inside this update loop
 (defn update-covid-workload!
