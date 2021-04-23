49973e37c1ad86ccea82e3a59f2c5e358d4844ed
Rex <rexwangcc@gmail.com>
2021-04-23 16:41:48 -0400

    Checkpoint.

--- api/src/wfl/module/covid.clj
+++ api/src/wfl/module/covid.clj
@@ -16,80 +16,20 @@
 
 (def pipeline "Sarscov2IlluminaFull")
 
-(defn ^:private add-workload-table!
-  "Use transaction `tx` to add a `CromwellWorkflow` table
-  for a `workload-request` running `workflow-wdl`."
-  [tx workload-request]
-  (let [{:keys [pipeline]} workload-request
-        create "CREATE TABLE %s OF CromwellWorkflow (PRIMARY KEY (id))"
-        setter "UPDATE workload SET pipeline = ?::pipeline WHERE id = ?"
-        [{:keys [id]}]
-        (-> workload-request
-          ;; FIXME: update the DB schema to write these new information in
-            (select-keys [:creator :executor :source :sink :project :labels :watchers])
-
-            (merge (select-keys (wfl/get-the-version) [:commit :version]))
-          ;; FIXME: how to write :release and :wdl
-            (assoc :release release :wdl path :uuid (UUID/randomUUID))
-            (->> (jdbc/insert! tx :workload)))
-        table (format "%s_%09d" pipeline id)]
-    (jdbc/execute!       tx [setter pipeline id])
-    (jdbc/db-do-commands tx [(format create table)])
-    (jdbc/update!        tx :workload {:items table} ["id = ?" id])
-    [id table]))
-
-;; TODO: implement COVID workload creation
-(defn create-covid-workload!
-  [tx {:keys [source sink executor] :as request}]
-  ;; TODO: validation
-  ;; TODO: dispatch on source/sink/executor
-  (-> request
-      (add-workload-table! tx)
-      (workloads/load-workload-for-id tx id)))
-
-(comment
-  (let [workload-request {:creator "ranthony@broadinstitute.org",
-                          :source
-                                     {:name "Terra DataRepo",
-                                      :dataset "sarscov2_illumina_full",
-                                      :table "inputs",
-                                      :snapshot "run_date"},
-                          :executor
-                                    {:name "Terra",
-                                     :workspace
-                                     "pathogen-genomic-surveillance/CDC_Viral_Sequencing",
-                                     :methodConfiguration
-                                     "pathogen-genomic-surveillance/sarscov2_illumina_full",
-                                     :version "3",
-                                     :entity "snapshot",
-                                     :fromSource "importSnapshot"},
-                          :sink
-                                    {:name "Terra Workspace",
-                                     :workspace
-                                     "pathogen-genomic-surveillance/CDC_Viral_Sequencing",
-                                     :entity "flowcell",
-                                     :fromOutputs {:column1 "output1"}},
-                          :labels
-                                    ["project:TestBoston"
-                                     "project:COVID-19 Surveillance"
-                                     "pipeline:sarscov2_illumina_full"],
-                          :watchers ["cloreth@broadinstitute.org"]}])
-
-  # Insert into DB
-  # Create table
-  (defn add-workload-table!
+#_(defn ^:private add-workload-table!
     "Use transaction `tx` to add a `CromwellWorkflow` table
-    for a `workload-request` running `workflow-wdl`."
-    [tx workflow-wdl workload-request]
-    (let [{:keys [path release]} workflow-wdl
-          {:keys [pipeline]} workload-request
+  for a `workload-request` running `workflow-wdl`."
+    [tx workload-request]
+    (let [{:keys [pipeline]} workload-request
           create "CREATE TABLE %s OF CromwellWorkflow (PRIMARY KEY (id))"
           setter "UPDATE workload SET pipeline = ?::pipeline WHERE id = ?"
           [{:keys [id]}]
           (-> workload-request
-              (select-keys [:creator :executor :input :output :project])
-              (update :executor util/de-slashify)
+          ;; FIXME: update the DB schema to write these new information in
+              (select-keys [:creator :executor :source :sink :project :labels :watchers])
+
               (merge (select-keys (wfl/get-the-version) [:commit :version]))
+          ;; FIXME: how to write :release and :wdl
               (assoc :release release :wdl path :uuid (UUID/randomUUID))
               (->> (jdbc/insert! tx :workload)))
           table (format "%s_%09d" pipeline id)]
@@ -97,7 +37,65 @@
       (jdbc/db-do-commands tx [(format create table)])
       (jdbc/update!        tx :workload {:items table} ["id = ?" id])
       [id table]))
-)
+
+;; TODO: implement COVID workload creation
+#_(defn create-covid-workload!
+    [tx {:keys [source sink executor] :as request}]
+  ;; TODO: validation
+  ;; TODO: dispatch on source/sink/executor
+    (-> request
+        (add-workload-table! tx)
+        (workloads/load-workload-for-id tx id)))
+
+(comment
+  (defn ^:private frob [[k v]]
+    (if (map? v)
+      [k (json/write-str v :escape-slash false)]
+      [k v]))
+
+  ;; Execution error (NoInitialContextException) at javax.naming.spi.NamingManager/getInitialContext (NamingManager.java:691).
+  ;; Need to specify class name in environment or system property, or in an application resource file: java.naming.factory.initial
+  (jdbc/with-db-transaction [tx (postgres/wfl-db-config)]
+    (let [workload-request {:creator  "ranthony@broadinstitute.org",
+                            :source
+                            {:name          "Terra DataRepo",
+                             :dataset       "sarscov2_illumina_full",
+                             :dataset_table "inputs",
+                             :snapshot      "run_date"},
+                            :executor
+                            {:name        "Terra",
+                             :workspace
+                             "pathogen-genomic-surveillance/CDC_Viral_Sequencing",
+                             :method_configuration
+                             "pathogen-genomic-surveillance/sarscov2_illumina_full",
+                             :version     "3",
+                             :entity      "snapshot",
+                             :from_source "importSnapshot"},
+                            :sink
+                            {:name         "Terra Workspace",
+                             :workspace
+                             "pathogen-genomic-surveillance/CDC_Viral_Sequencing",
+                             :entity       "flowcell",
+                             :from_outputs {:column1 "output1"}},
+                            :labels
+                            ["project:TestBoston"
+                             "project:COVID-19 Surveillance"
+                             "pipeline:sarscov2_illumina_full"],
+                            :watchers ["cloreth@broadinstitute.org"]}]
+      (letfn [(frob [[k v]] (if (map? v) [k (json/write-str v :escape-slash false)] [k v]))
+              (go! [k]
+                (-> workload-request
+                    k
+                    (->> (map frob)
+                         (into {}))
+                    wfl.debug/trace
+                    (jdbc/insert! tx k)))]
+
+        (run! go! [:source :executor :sink]))))
+
+(defn start-covid-workload!
+  ""
+  [])
 
 ;; TODO: implement progressive (private) functions inside this update loop
 (defn update-covid-workload!
