ce9dc4e167c59dfcfe4e3d71865ef964ef3114cd
rhiananthony <ranthony@broadinstitute.org>
2021-04-28 14:24:03 -0400

    Initial creat covid workload commit

---
+++ api/test/wfl/integration/modules/covid_test.clj
@@ -0,0 +1,18 @@
+(ns wfl.integration.modules.covid-test
+  (:require [clojure.test :refer :all]
+            [clojure.test :as clj-test]
+            [wfl.tools.fixtures :as fixtures]
+            [wfl.jdbc :as jdbc]
+            [wfl.api.workloads :as workloads]))
+
+(clj-test/use-fixtures :once fixtures/temporary-postgresql-database)
+
+(deftest test-create-covid-workflow
+  (jdbc/with-db-transaction [tx (fixtures/testing-db-config)]
+    (workloads/create-workload! tx {:pipeline "Sarscov2IlluminaFull"})
+  ))
+
+(comment
+  (wfl.tools.fixtures/create-local-database-for-testing (wfl.tools.fixtures/testing-db-config))
+  )
+
