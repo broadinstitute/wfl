24ad12708f210ea401cb0b6cec2c7612dd94c168
Rex <rexwangcc@gmail.com>
2021-04-30 15:23:04 -0400

    Fix and rewrite the test.

--- api/test/wfl/integration/modules/covid_test.clj
+++ api/test/wfl/integration/modules/covid_test.clj
@@ -2,23 +2,20 @@
   (:require [clojure.test :refer :all]
             [clojure.test :as clj-test]
             [wfl.tools.fixtures :as fixtures]
-            [wfl.jdbc :as jdbc]
             [wfl.tools.workloads :as workloads]))
 
 (clj-test/use-fixtures :once fixtures/temporary-postgresql-database)
+
 (def ^:private testing-dataset "ff6e2b40-6497-4340-8947-2f52a658f561")
 (def ^:private testing-workspace "general-dev-billing-account/test-snapshots")
 (def ^:private testing-method-configuration "pathogen-genomic-surveillance/sarscov2_illumina_full")
 
-(deftest test-create-covid-workload
-  (jdbc/with-db-transaction [tx (fixtures/testing-db-config)]
-    (workloads/create-workload! {:pipeline "Sarscov2IlluminaFull"
-                                 :source {:name "Terra DataRepo",
-                                          :dataset testing-dataset}
-                                 :executor {:name "Terra",
-                                            :method_configuration testing-method-configuration}
-                                 :sink {:name "Terra Workspace",
-                                        :workspace testing-workspace}})))
+(defn ^:private make-covid-workload-request []
+  (-> (workloads/covid-workload-request testing-dataset testing-method-configuration testing-workspace)
+    (assoc :creator @workloads/email)))
 
-(comment
-  (wfl.tools.fixtures/create-local-database-for-testing (wfl.tools.fixtures/testing-db-config)))
+(deftest test-create-covid-workload
+  (testing "COVID workload-request can pass verification"
+    ;; NOTE: COVID workload creation returns nil until it's fully implemented
+    (let [workload (workloads/create-workload! (make-covid-workload-request))]
+      (is (nil? workload)))))
