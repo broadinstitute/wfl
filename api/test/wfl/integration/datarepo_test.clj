1056909ec4f2bbe340304a10ebef904ec28e836a
Edmund Higham <edhigham@gmail.com>
2021-04-30 12:05:03 -0400

    fix rawls test failure

--- api/test/wfl/integration/datarepo_test.clj
+++ api/test/wfl/integration/datarepo_test.clj
@@ -60,8 +60,7 @@
                          workflows/make-object-type)
         workflow-id  (UUID/randomUUID)]
     (fixtures/with-fixtures
-      [(fixtures/with-temporary-cloud-storage-folder
-         "broad-gotc-dev-wfl-ptc-test-inputs")
+      [(fixtures/with-temporary-cloud-storage-folder fixtures/gcs-test-bucket)
        (fixtures/with-temporary-dataset
          (datasets/unique-dataset-request tdr-profile dataset-json))]
       (fn [[temp dataset]]
--- api/test/wfl/integration/rawls_test.clj
+++ api/test/wfl/integration/rawls_test.clj
@@ -1,36 +1,34 @@
 (ns wfl.integration.rawls-test
-  (:require [clojure.test :refer [deftest is testing]]
-            [wfl.service.rawls :as rawls]
-            [wfl.util :as util])
-  (:import [clojure.lang ExceptionInfo]
-           [java.util UUID]))
-
-;; Of the form NAMESPACE/NAME
-(def workspace "general-dev-billing-account/test-snapshots")
+  (:require [clojure.test       :refer [deftest is testing use-fixtures]]
+            [wfl.service.rawls  :as rawls]
+            [wfl.tools.fixtures :as fixtures])
+  (:import [clojure.lang ExceptionInfo]))
 
 ;; A known Terra Data Repository snapshot's ID...
-(def snapshot-id "7cb392d8-949b-419d-b40b-d039617d2fc7")
+(def snapshot-name "snapshot")
+(def snapshot-id   "7cb392d8-949b-419d-b40b-d039617d2fc7")
 
-;; And a known link to the above TDR snapshot in the workspace.
-(def known-snapshot-link {:name "snapshot" :referenceId "2d15f9bd-ecb9-46b3-bb6c-f22e20235232"})
+(let [new-env {"WFL_FIRECLOUD_URL"
+               "https://firecloud-orchestration.dsde-dev.broadinstitute.org"}]
+  (use-fixtures :once (fixtures/temporary-environment new-env)))
 
-(deftest test-get-snapshot
-  (let [snapshot (rawls/get-snapshot workspace (:referenceId known-snapshot-link))]
-    (is (= "DATA_REPO_SNAPSHOT" (:referenceType snapshot)))
-    (is (= snapshot-id (get-in snapshot [:reference :snapshot])))
-    (is (= (:name known-snapshot-link) (:name snapshot)))))
-
-(deftest test-create-snapshot-reference
-  (testing "Creating snapshot reference with same name of existing throws 409 error"
-    (is (thrown-with-msg? ExceptionInfo #"clj-http: status 409"
-                          (rawls/create-snapshot-reference workspace
-                                                           snapshot-id
-                                                           (:name known-snapshot-link)))))
-  (let [name (str (UUID/randomUUID))]
-    (util/bracket
-     #(rawls/create-snapshot-reference workspace snapshot-id name "wfl.rawls-test/test-create-snapshot")
-     #(rawls/delete-snapshot workspace %)
-     #(let [snapshot (rawls/get-snapshot workspace %)]
-        (is (= "DATA_REPO_SNAPSHOT" (:referenceType snapshot)))
-        (is (= snapshot-id (get-in snapshot [:reference :snapshot])))
-        (is (= name (:name snapshot)))))))
+(deftest test-snapshot-references
+  (fixtures/with-temporary-workspace
+    "general-dev-billing-account/test-workspace"
+    "hornet-eng"
+    (fn [workspace]
+      (let [reference-id
+            (testing "Create"
+              (let [snapshot (rawls/create-snapshot-reference workspace snapshot-id snapshot-name)]
+                (is (= "DATA_REPO_SNAPSHOT" (:referenceType snapshot)))
+                (is (= snapshot-id (get-in snapshot [:reference :snapshot])))
+                (is (= snapshot-name (:name snapshot)))
+                (:referenceId snapshot)))]
+        (testing "Get"
+          (let [snapshot (rawls/get-snapshot workspace reference-id)]
+            (is (= "DATA_REPO_SNAPSHOT" (:referenceType snapshot)))
+            (is (= snapshot-id (get-in snapshot [:reference :snapshot])))
+            (is (= snapshot-name (:name snapshot)))))
+        (testing "Create already exists"
+          (is (thrown-with-msg? ExceptionInfo #"clj-http: status 409"
+                                (rawls/create-snapshot-reference workspace snapshot-id snapshot-name))))))))
