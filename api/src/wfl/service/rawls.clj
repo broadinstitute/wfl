9853309ae7115952c2bad3b662d02933fffb6c99
Olivia Kotsopoulos <okotsopo@broadinstitute.org>
2021-05-05 16:31:53 -0400

    [GH-1215] Import snapshots within COVID workload (#376)
    - Data model change: a snapshot_reference_id is linked to a workspace and should be associated with the executor, not the source (TDR in our use case)
    - Added covid/get-imported-snapshot-reference - nil or snapshot reference from Rawls for snapshot_reference_id in executor details instance (to be created from TerraExecutorDetails)
    - Added covid/import-snapshot! - import snapshot to workspace, writing to DB if successful
    - Added integration tests (incomplete coverage)

--- api/src/wfl/service/rawls.clj
+++ api/src/wfl/service/rawls.clj
@@ -7,6 +7,7 @@
             [clojure.string :as str]
             [wfl.auth :as auth]
             [wfl.environment :as env]
+            [wfl.service.datarepo :as datarepo]
             [wfl.util :as util]))
 
 (defn ^:private rawls-url [& parts]
@@ -22,8 +23,8 @@
       util/response-body-json))
 
 (defn create-snapshot-reference
-  "Link a Terra Data Repo snapshot with id SNAPSHOT-ID to a fully-qualified
-  Terra WORKSPACE as NAME, optionally described by DESCRIPTION."
+  "Link SNAPSHOT-ID to WORKSPACE as NAME with DESCRIPTION.
+  If NAME unspecified, generate a unique reference name from the snapshot name."
   ([workspace snapshot-id name description]
    (-> (workspace-api-url workspace "snapshots")
        (http/post {:headers      (auth/get-auth-header)
@@ -34,15 +35,18 @@
                                                  :escape-slash false)})
        util/response-body-json))
   ([workspace snapshot-id name]
-   (create-snapshot-reference workspace snapshot-id name "")))
+   (create-snapshot-reference workspace snapshot-id name ""))
+  ([workspace snapshot-id]
+   (let [name (util/randomize (:name (datarepo/snapshot snapshot-id)))]
+     (create-snapshot-reference workspace snapshot-id name))))
 
-(defn get-snapshot
-  "Return the snapshot in fully-qualified Terra WORKSPACE with REFERENCE-ID."
+(defn get-snapshot-reference
+  "Return the snapshot reference in fully-qualified Terra WORKSPACE with REFERENCE-ID."
   [workspace reference-id]
   (get-workspace-json workspace "snapshots" reference-id))
 
-(defn delete-snapshot
-  "Delete the snapshot in fully-qualified Terra WORKSPACE with REFERENCE-ID."
+(defn delete-snapshot-reference
+  "Delete the snapshot reference in fully-qualified Terra WORKSPACE with REFERENCE-ID."
   [workspace reference-id]
   (-> (workspace-api-url workspace "snapshots" reference-id)
       (http/delete {:headers (auth/get-auth-header)})
