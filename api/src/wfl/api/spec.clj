1e3a4f4bfa8b8b2fef10fa961d09859c5045d3b4
Tom Lyons <tbl3rd@gmail.com>
2021-05-17 14:32:10 -0400

    Restore batch or covid request.

--- api/src/wfl/api/spec.clj
+++ api/src/wfl/api/spec.clj
@@ -159,12 +159,12 @@
                                  ::dataset
                                  ::table]))
 
-(s/def ::covid-workload-request (s/keys :req-un [::executor
+(s/def ::covid-workload-request (s/keys :req-un [::source
+                                                 ::sink
                                                  ::pipeline
-                                                 ::sink]
+                                                 ::executor]
                                         :opt-un [::labels
                                                  ::project
-                                                 ::source
                                                  ::watchers]))
 
 (s/def ::covid-workload-response (s/keys :opt-un [::finished
@@ -172,9 +172,8 @@
                                                   ::started
                                                   ::stopped]))
 
-(s/def ::workload-request ::covid-workload-request
-  #_(s/or :batch ::batch-workload-request
-          :covid ::covid-workload-request))
+(s/def ::workload-request (s/or :batch ::batch-workload-request
+                                :covid ::covid-workload-request))
 
 (s/def ::workload-response (s/or :batch ::batch-workload-response
                                  :covid ::covid-workload-response))
