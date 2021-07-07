1162540d1fcdb8c8270026495e46691071d63a54
Edmund Higham <edhigham@gmail.com>
2021-07-07 16:21:46 -0400

    fix lint failure

--- api/src/wfl/environment.clj
+++ api/src/wfl/environment.clj
@@ -30,33 +30,33 @@
         (json/write-str :escape-slash false)
         .getBytes)
    "WFL_CLIO_URL"
-   #(-> "https://clio.gotc-dev.broadinstitute.org")
+   (fn [] "https://clio.gotc-dev.broadinstitute.org")
    "WFL_COOKIE_SECRET"
    #(-> "secret/dsde/gotc/dev/zero" vault-secrets :cookie_secret)
    "WFL_TDR_URL"
-   #(-> "https://data.terra.bio")
+   (fn [] "https://data.terra.bio")
    "WFL_OAUTH2_CLIENT_ID"
    #(-> "secret/dsde/gotc/dev/zero" vault-secrets :oauth2_client_id)
    "WFL_POSTGRES_PASSWORD"
-   #(-> "password")
+   (fn [] "password")
    "WFL_POSTGRES_URL"
-   #(-> "jdbc:postgresql:wfl")
+   (fn [] "jdbc:postgresql:wfl")
    "WFL_POSTGRES_USERNAME"
-   #(-> nil)
+   (fn [] nil)
    "WFL_FIRECLOUD_URL"
-   #(-> "https://api.firecloud.org")
+   (fn [] "https://api.firecloud.org")
    "WFL_RAWLS_URL"
-   #(-> "https://rawls.dsde-dev.broadinstitute.org")
+   (fn [] "https://rawls.dsde-dev.broadinstitute.org")
    "WFL_DOCKSTORE_URL"
-   #(-> "https://dockstore.org")
+   (fn [] "https://dockstore.org")
 
    ;; -- variables used in test code below this line --
    "WFL_CROMWELL_URL"
-   #(-> "https://cromwell-gotc-auth.gotc-dev.broadinstitute.org")
+   (fn [] "https://cromwell-gotc-auth.gotc-dev.broadinstitute.org")
    "WFL_TDR_DEFAULT_PROFILE"
-   #(-> "6370f5a1-d777-4991-8200-ceab83521d43")
+   (fn [] "6370f5a1-d777-4991-8200-ceab83521d43")
    "WFL_WFL_URL"
-   #(-> "https://dev-wfl.gotc-dev.broadinstitute.org")})
+   (fn [] "https://dev-wfl.gotc-dev.broadinstitute.org")})
 
 (def ^:private __getenv
   (memoize #(or (System/getenv %) (when-let [init (defaults %)] (init)))))
