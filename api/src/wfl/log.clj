f2b1e21d468ca85cba5c611de33f3daf49fea95c
rfricke-asymmetrik <rfricke@asymmetrik.com>
2021-07-07 11:50:50 -0400

    Some updates to log namespace

--- api/src/wfl/log.clj
+++ api/src/wfl/log.clj
@@ -1,72 +1,54 @@
 (ns wfl.log
   "Logging for WFL"
   (:require [clojure.core]
-            [clojure.data.json :refer [write-str]])
+            [clojure.data.json :refer [write-str]]
+            [clojure.string :refer [upper-case]])
   (:import [java.sql Timestamp]
            [java.time OffsetDateTime]))
 
-(defn googleize-field
+(defn ^:private googleize-field
   "Prepends the google logging domain to the fields that require it for Google Logging."
   [field]
   (let [field-name (name field)]
-    (if (some #(= field-name %) '("labels" "insertId" "operation" "sourceLocation" "spanId" "trace")) (str "logging.googleapis.com/" (name field-name))
-        field-name)))
-
-(defn write-to-stdout
-  "Writes the output to standard output as a json string."
-  [json]
-  (-> (write-str json :key-fn googleize-field :escape-slash false)
-      (.toString)
-      (println)))
-
-(defn get-timestamp
-  "Gets timestamp for the json"
-  []
-  (Timestamp/from (.toInstant (OffsetDateTime/now))))
-
-(defn add-common-fields
-  "Builds the common message before applying type specific fields."
-  [json message]
-  (merge json
-         {:message message
-          :timestamp (get-timestamp)}))
+    (if (#{"labels" "insertId" "operation" "sourceLocation" "spanId" "trace"})
+      (str "logging.googleapis.com/" (name field-name))
+      field-name)))
 
 (defn log
   "Write a log entry to standard output."
   [severity message & {:as additional-fields}]
-  (let [severity-upper (.toUpperCase severity)]
-    (if (not (some #(= severity-upper %) '("DEFAULT" "DEBUG" "INFO" "NOTICE" "WARNING" "ERROR" "CRITICAL" "ALERT" "EMERGENCY")))
-      (throw (ex-info "Invalid severity provided to logging function." {:severity severity-upper}))
-      (->> (add-common-fields {:severity severity-upper} message)
-           (merge additional-fields)
-           (write-to-stdout)))))
+  (let [severity-upper (-> severity name upper-case)]
+    (-> (merge {:severity severity-upper
+                :message message
+                :timestamp (Timestamp/from (.toInstant (OffsetDateTime/now)))}
+               additional-fields)
+        (write-str :key-fn googleize-field :espace-slash false)
+        (.toString)
+        (println))))
 
 (defmacro info
   "Log as Information"
   [message & more]
-  `(->> (print-str ~message ~@more)
-        (log "INFO")))
+  `(log :info (print-str ~message ~@more)))
 
 (defmacro warn
   "Log as a Warning"
   [message & more]
-  `(->> (print-str ~message ~@more)
-        (log "WARNING")))
+  `(log :warning (print-str ~message ~@more)))
 
 (defmacro debug
   "Log as Debug"
   [message & more]
-  `(->> (print-str ~message ~@more)
-        (log "DEBUG")))
+  `(log :debug (print-str ~message ~@more)))
 
 (defmacro error
   "Log as Error"
   [message & more]
-  `(->> (print-str ~message ~@more)
-        (log "ERROR")))
+  `(log :error (print-str ~message ~@more)))
+
 (comment
   (info "my message" "with" "stuff")
   (warn "my message")
   (debug "my message")
   (error "my message")
-  (log "INFO" "test" :labels {:test "HI"}))
+  (log :info "test" :labels {:test "HI"}))
