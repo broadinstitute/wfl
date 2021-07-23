cb60b104e3bf2a22bbd1eb6f4350aab273ca0855
rfricke-asymmetrik <rfricke@asymmetrik.com>
2021-07-23 11:40:11 -0400

    unit testing for logging levels

--- api/test/wfl/unit/logging_test.clj
+++ api/test/wfl/unit/logging_test.clj
@@ -4,7 +4,7 @@
    [wfl.log :as log]
    [clojure.data.json :refer [read-str]]
    [clojure.test :refer [is deftest testing]]
-   [clojure.string :refer [upper-case]])
+   [clojure.string :refer [upper-case blank?]])
   (:import java.util.regex.Pattern))
 
 (defmulti check-message?
@@ -25,11 +25,31 @@
     (and (= (get-in json ["severity"]) (-> severity name upper-case))
          (boolean (check-message? test (get-in json ["message"]))))))
 
+(deftest to-logging-level-edn-test
+  (testing "test the to-logging-level-edn function produces the correct end"
+    (let [info-level (log/to-logging-level-edn :info)
+          warning-level (log/to-logging-level-edn :warning)
+          error-level (log/to-logging-level-edn :error)]
+      (is (= false (get info-level :debug)))
+      (is (= true (get info-level :info)))
+      (is (= true (get info-level :notice)))
+      (is (= false (get warning-level :info)))
+      (is (= true (get warning-level :warning)))
+      (is (= false (get error-level :warning)))
+      (is (= true (get error-level :error))))))
+
 ;; Useful information on this file is in docs/logging.md#Testing
 
 (deftest level-test
   (testing "basic logging levels"
-    (is (logged? (with-out-str (log/info "Hello World!")) :info "Hello World!"))
-    (is (logged? (with-out-str (log/warn "This is a warning")) :warning "This is a warning"))
-    (is (logged? (with-out-str (log/error "This is an error")) :error "This is an error"))
-    (is (logged? (with-out-str (log/debug "This is just a debugging message")) :debug "This is just a debugging message"))))
+    (with-redefs [log/logging-level (atom (log/to-logging-level-edn :debug))]
+      (is (logged? (with-out-str (log/info "Hello World!")) :info "Hello World!"))
+      (is (logged? (with-out-str (log/warn "This is a warning")) :warning "This is a warning"))
+      (is (logged? (with-out-str (log/error "This is an error")) :error "This is an error"))
+      (is (logged? (with-out-str (log/debug "This is just a debugging message")) :debug "This is just a debugging message")))))
+
+(deftest severity-level-filtering-test
+  (testing "test current logging level correctly ignores lesser levels"
+    (with-redefs [log/logging-level (atom (log/to-logging-level-edn :info))]
+      (is (blank? (with-out-str (log/debug "Debug Message"))))
+      (is (logged? (with-out-str (log/info "Info Message")) :info "Info Message")))))
