ec132aec1b4347be0187485e047f310004ab1098
Tom Lyons <tbl3rd@gmail.com>
2022-01-24 17:50:02 -0500

    Hide some GCP details.

--- api/src/wfl/log.clj
+++ api/src/wfl/log.clj
@@ -1,33 +1,46 @@
 (ns wfl.log
-  "Logging for WFL. The severity levels provided here are based off of
-   the severity levels supported by GCP's Stackdriver. A list of the supported
-   levels can be found here:
-   https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#LogSeverity"
+  "GCP Stackdriver logging for WFL."
   (:require [clojure.data.json  :as json]
             [clojure.spec.alpha :as s]
             [clojure.string     :as str])
   (:import [java.time Instant]))
 
-(def levels
-  "A list of the available logging levels the application can use."
-  [:debug :info :notice :warning :error :alert :critical :emergency])
+(alias 'lgc (ns-name (create-ns 'logging.googleapis.com)))
 
-(defn levels-contains?
-  "Validate that the provided label exists."
-  [s]
-  (let [level (-> s str/lower-case keyword)]
-    (some #(= level %) levels)))
-(s/def ::level (s/and string? levels-contains?))
-(s/def ::logging-level-request (s/keys :req-un [::level]))
-(s/def ::logging-level-response (s/keys :req-un [::level]))
+;; https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#LogSeverity
+;;
+(def ^:private level?
+  "The set of log severity level keywords."
+  #{:debug :info :notice :warning :error :critical :alert :emergency})
+
+(defn ^:private level-string?
+  "True when `level` string names a log severity level."
+  [level]
+  (some level? (-> level str/lower-case keyword)))
+
+(s/def ::level-string   (s/and string? level-string?))
+(s/def ::level-request  (s/keys :req-un [::level-string]))
+(s/def ::level-response (s/keys :req-un [::level-string]))
+
+;; https://cloud.google.com/logging/docs/agent/logging/configuration#special-fields
+;;
+(def google-fields
+  "Map WFL's log field names to what Stackdriver recognizes."
+  {::httpRequest :httpRequest
+   ::insertId    ::lgc/insertId
+   ::labels      ::lgc/labels
+   ::operation   ::lgc/operation
+   ::spanId      ::lgc/spanId
+   ::trace       ::lgc/trace})
 
 (defn ^:private key-fn
   "Preserve the namespace of `key` when qualified."
   [key]
   ((:key-fn json/default-write-options)
-   (if (qualified-keyword? key)
-     (str (namespace key) \/ (name key))
-     key)))
+   (let [googled (google-fields key key)]
+     (if (qualified-keyword? googled)
+       (str (namespace googled) \/ (name googled))
+       key))))
 
 ;; HACK: Override how JSONWriter handles Throwables.
 ;; json/write-object is private and handles java.util.Map.
@@ -68,31 +81,7 @@
   (atom :info))
 
 (defmacro log
-  "Log `expression` with `severity` and optional fields to provide more
-  information in a logging message.
-
-  An explanation of the fields can be found here:
-  https://cloud.google.com/logging/docs/agent/logging/configuration#special-fields
-
-  :httpRequest
-  A structured record for the Http Request that was made
-
-  :logging.googleapis.com/insertId
-  Logs with the same insertId and timestamp
-  are considered duplicates in Google Logging.
-
-  :logging.googleapis.com/labels
-  A map of key value pairs that can be searched on in Google Logging.
-
-  :logging.googleapis.com/operation
-  Additional information about a potentially long-running operation
-  with which a log entry is associated.
-
-  :logging.googleapis.com/spanId
-  The span ID within the trace associated with the log entry.
-
-  :logging.googleapis.com/trace
-  Resource name of the trace associated with the log entry if any."
+  "Log `expression` with `severity` and `additional-fields`."
   [severity expression & {:as additional-fields}]
   (let [{:keys [line]} (meta &form)]
     `(let [x# ~expression
@@ -102,7 +91,7 @@
                  (merge {:timestamp (Instant/now)
                          :severity ~(-> severity name str/upper-case)
                          :message x#
-                         :logging.googleapis.com/sourceLocation
+                         ::lgc/sourceLocation
                          {:file ~*file* :line ~line}}
                         ~additional-fields)))
        nil)))
@@ -111,29 +100,29 @@
   "Log `expression` for debugging.
    This is for debug or trace information."
   ([expression & {:as labels}]
-   `(log :debug ~expression :logging.googleapis.com/labels ~labels)))
+   `(log :debug ~expression ::lgc/labels ~labels)))
 
 (defmacro info
   "Log `expression` as information.
    Used for routine information, such as ongoing status or performance."
   ([expression & {:as labels}]
-   `(log :info ~expression :logging.googleapis.com/labels ~labels)))
+   `(log :info ~expression ::lgc/labels ~labels)))
 
 (defmacro notice
   "Log `expression` as a notice.
    Used for normal but significant events, such as start up,
    shut down, or a configuration change."
   ([expression & {:as labels}]
-   `(log :notice ~expression :logging.googleapis.com/labels ~labels)))
+   `(log :notice ~expression ::lgc/labels ~labels)))
 
 (defmacro warn
   "Log `expression` as a warning.
    Used for warning events, which might cause problems."
   ([expression & {:as labels}]
-   `(log :warning ~expression :logging.googleapis.com/labels ~labels)))
+   `(log :warning ~expression ::lgc/labels ~labels)))
 
 (defmacro error
   "Log `expression` as an error.
    Used for events that are likely to cause problems."
   ([expression & {:as labels}]
-   `(log :error ~expression :logging.googleapis.com/labels ~labels)))
+   `(log :error ~expression ::lgc/labels ~labels)))
