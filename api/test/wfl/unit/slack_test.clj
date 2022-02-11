f72da8a5efeb1c7178a3bb6ba26e626c2a1caa55
Olivia Kotsopoulos <okotsopo@broadinstitute.org>
2022-02-11 17:23:40 -0500

    [GH-1608] Release WFL v0.14.1 (#572)
    - GH-1604 Disable / hide Slacking behind WFL_SLACK_ENABLED (#571)

--- api/test/wfl/unit/slack_test.clj
+++ api/test/wfl/unit/slack_test.clj
@@ -1,6 +1,8 @@
 (ns wfl.unit.slack-test
-  (:require [clojure.test      :refer [deftest is testing]]
-            [wfl.service.slack :as slack])
+  (:require [clojure.string     :as str]
+            [clojure.test       :refer [deftest is testing]]
+            [wfl.service.slack  :as slack]
+            [wfl.tools.fixtures :as fixtures])
   (:import [clojure.lang PersistentQueue]))
 
 (def ^:private testing-agent (agent (PersistentQueue/EMPTY)))
@@ -14,3 +16,26 @@
         (slack/add-notification testing-agent (testing-slack-notification)))
       (await testing-agent)
       (is (= num-msg (count (seq @testing-agent)))))))
+
+(deftest test-notify-watchers-only-when-enabled
+  (let [notification (testing-slack-notification)
+        workload     {:uuid     "workload-uuid"
+                      :watchers [["slack" (:channel notification)]]}
+        message      (:message notification)]
+    (letfn [(mock-add-notification [maybe-enabled]
+              (fn [_notifier payload]
+                (if (= "enabled" maybe-enabled)
+                  (do (is (= (:channel notification) (:channel payload)))
+                      (is (str/includes? (:message payload) message)))
+                  (throw (ex-info "Should not notify"
+                                  {:maybe-enabled maybe-enabled})))))
+            (verify [maybe-enabled]
+              (fixtures/with-temporary-environment
+                {slack/enabled-env-var-name maybe-enabled}
+                #(with-redefs-fn
+                   {#'slack/add-notification (mock-add-notification maybe-enabled)}
+                   (fn [] (slack/notify-watchers workload message)))))]
+      (testing "notifications emitted when feature enabled"
+        (verify "enabled"))
+      (testing "notifications not emitted when feature disabled"
+        (verify "any-other-value-disables-slacking")))))
