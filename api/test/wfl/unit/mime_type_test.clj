2c19d4c88f08b768ab1ba16a5d1881574d4502d2
Tom Lyons <tbl3rd@gmail.com>
2022-05-17 19:23:06 -0400

    Simplfy.

--- api/test/wfl/unit/mime_type_test.clj
+++ api/test/wfl/unit/mime_type_test.clj
@@ -1,9 +1,7 @@
 (ns wfl.unit.mime-type-test
+  "Test wfl.mime-type."
   (:require [clojure.test        :refer [deftest is]]
-            [clojure.string      :as str]
-            [wfl.mime-type       :as mime-type]
-            [wfl.tools.resources :as resources]
-            [wfl.tools.workflows :as workflows]))
+            [wfl.mime-type       :as mime-type]))
 
 (deftest test-mime-types
   (let [cases [[".bam"   "application/octet-stream"]
@@ -19,25 +17,3 @@
     (doseq [[filename expected] cases]
       (is (= expected (mime-type/ext-mime-type filename))
           (str filename " does not have the expected mime-type")))))
-
-(deftest test-workflow-mime-types
-  (let [exclude ["gs://fc-f9083173-1e35-4c1b-9d80-d837d6d12283"
-                 "475d0a1d-20c0-42a1-968a-7540b79fcf0c"
-                 "sarscov2_illumina_full"
-                 "2768b29e-c808-4bd6-a46b-6c94fd2a67aa"
-                 "call-demux_deplete/demux_deplete"
-                 "7045e3bd-cf81-401d-b7da-4eee334cc937"
-                 "call-sra_meta_prep"
-                 "cacheCopy"
-                 "write_lines_0128f217b922021a3b05cdc678f982d2.tmp"]
-        exclude? #{(str/join "/" exclude)}
-        cases [[(resources/read-resource "assemble_refbased/outputs.edn")
-                (-> "assemble_refbased.edn" resources/read-resource :outputs)]
-               [(resources/read-resource "sarscov2_illumina_full/outputs.edn")
-                (-> "sarscov2_illumina_full.edn" resources/read-resource :outputs)]]]
-    (doseq [[values description] cases]
-      (let [type (workflows/make-object-type description)]
-        (doseq [filename (workflows/get-files [type values])]
-          (let [ext (mime-type/ext-mime-type-no-default filename)]
-            (is (or (some? ext) (exclude? filename))
-                (str filename " does not have a mime-type"))))))))
