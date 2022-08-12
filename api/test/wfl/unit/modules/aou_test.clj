5502bf33aca648cd84d03e943643c938c2278d5d
Tom Lyons <tbl3rd@gmail.com>
2022-08-12 14:55:10 -0400

    Fix names.

--- api/test/wfl/unit/modules/aou_test.clj
+++ api/test/wfl/unit/modules/aou_test.clj
@@ -35,68 +35,68 @@
           "label map is not made as expected"))))
 
 (deftest test-aou-inputs-preparation
-  (let [expected-per-sample-inputs         {:analysis_version_number     "foo"
-                                            :bead_pool_manifest_file     "foo"
-                                            :call_rate_threshold         "foo"
-                                            :chip_well_barcode           "foo"
-                                            :cluster_file                "foo"
-                                            :extended_chip_manifest_file "foo"
-                                            :gender_cluster_file         "foo"
-                                            :green_idat_cloud_path       "foo"
-                                            :minor_allele_frequency_file "foo"
-                                            :params_file                 "foo"
-                                            :red_idat_cloud_path         "foo"
-                                            :reported_gender             "foo"
-                                            :sample_alias                "foo"
-                                            :sample_lsid                 "foo"
-                                            :zcall_thresholds_file       "foo"
-                                            :environment                 "foo"}
-        redundant-per-sample-inputs-inputs (merge expected-per-sample-inputs
-                                                  {:extra "bar"})
-        missing-per-sample-inputs-inputs   (dissoc expected-per-sample-inputs
-                                                   :analysis_version_number)
-        all-expected-keys-no-control       #{:Arrays.preemptible_tries
-                                             :Arrays.environment
-                                             :Arrays.ref_dict
-                                             :Arrays.params_file
-                                             :Arrays.subsampled_metrics_interval_list
-                                             :Arrays.chip_well_barcode
-                                             :Arrays.sample_alias
-                                             :Arrays.variant_rsids_file
-                                             :Arrays.ref_fasta_index
-                                             :Arrays.dbSNP_vcf
-                                             :Arrays.disk_size
-                                             :Arrays.contamination_controls_vcf
-                                             :Arrays.green_idat_cloud_path
-                                             :Arrays.minor_allele_frequency_file
-                                             :Arrays.fingerprint_genotypes_vcf_index_file
-                                             :Arrays.vault_token_path
-                                             :Arrays.reported_gender
-                                             :Arrays.dbSNP_vcf_index
-                                             :Arrays.extended_chip_manifest_file
-                                             :Arrays.zcall_thresholds_file
-                                             :Arrays.sample_lsid
-                                             :Arrays.red_idat_cloud_path
-                                             :Arrays.gender_cluster_file
-                                             :Arrays.ref_fasta
-                                             :Arrays.bead_pool_manifest_file
-                                             :Arrays.analysis_version_number
-                                             :Arrays.fingerprint_genotypes_vcf_file
-                                             :Arrays.cluster_file
-                                             :Arrays.call_rate_threshold
-                                             :Arrays.haplotype_database_file}
-        all-expected-keys                   (set/union
-                                             all-expected-keys-no-control
-                                             #{:Arrays.control_sample_vcf_index_file
-                                               :Arrays.control_sample_intervals_file
-                                               :Arrays.control_sample_vcf_file
-                                               :Arrays.control_sample_name})]
+  (let [expected-per-sample-inputs   {:analysis_version_number     "foo"
+                                      :bead_pool_manifest_file     "foo"
+                                      :call_rate_threshold         "foo"
+                                      :chip_well_barcode           "foo"
+                                      :cluster_file                "foo"
+                                      :extended_chip_manifest_file "foo"
+                                      :gender_cluster_file         "foo"
+                                      :green_idat_cloud_path       "foo"
+                                      :minor_allele_frequency_file "foo"
+                                      :params_file                 "foo"
+                                      :red_idat_cloud_path         "foo"
+                                      :reported_gender             "foo"
+                                      :sample_alias                "foo"
+                                      :sample_lsid                 "foo"
+                                      :zcall_thresholds_file       "foo"
+                                      :environment                 "foo"}
+        redundant-per-sample-inputs  (merge expected-per-sample-inputs
+                                            {:extra "bar"})
+        missing-per-sample-inputs    (dissoc expected-per-sample-inputs
+                                             :analysis_version_number)
+        all-expected-keys-no-control #{:Arrays.preemptible_tries
+                                       :Arrays.environment
+                                       :Arrays.ref_dict
+                                       :Arrays.params_file
+                                       :Arrays.subsampled_metrics_interval_list
+                                       :Arrays.chip_well_barcode
+                                       :Arrays.sample_alias
+                                       :Arrays.variant_rsids_file
+                                       :Arrays.ref_fasta_index
+                                       :Arrays.dbSNP_vcf
+                                       :Arrays.disk_size
+                                       :Arrays.contamination_controls_vcf
+                                       :Arrays.green_idat_cloud_path
+                                       :Arrays.minor_allele_frequency_file
+                                       :Arrays.fingerprint_genotypes_vcf_index_file
+                                       :Arrays.vault_token_path
+                                       :Arrays.reported_gender
+                                       :Arrays.dbSNP_vcf_index
+                                       :Arrays.extended_chip_manifest_file
+                                       :Arrays.zcall_thresholds_file
+                                       :Arrays.sample_lsid
+                                       :Arrays.red_idat_cloud_path
+                                       :Arrays.gender_cluster_file
+                                       :Arrays.ref_fasta
+                                       :Arrays.bead_pool_manifest_file
+                                       :Arrays.analysis_version_number
+                                       :Arrays.fingerprint_genotypes_vcf_file
+                                       :Arrays.cluster_file
+                                       :Arrays.call_rate_threshold
+                                       :Arrays.haplotype_database_file}
+        all-expected-keys             (set/union
+                                       all-expected-keys-no-control
+                                       #{:Arrays.control_sample_vcf_index_file
+                                         :Arrays.control_sample_intervals_file
+                                         :Arrays.control_sample_vcf_file
+                                         :Arrays.control_sample_name})]
     (testing "aou filters out non-necessary keys for per-sample-inputs"
       (is (= expected-per-sample-inputs (aou/get-per-sample-inputs
-                                         redundant-per-sample-inputs-inputs))))
+                                         redundant-per-sample-inputs))))
     (testing "aou throws for missing keys for per-sample-inputs"
       (is (thrown? Exception (aou/get-per-sample-inputs
-                              missing-per-sample-inputs-inputs))))
+                              missing-per-sample-inputs))))
     (testing "aou prepares all necessary keys"
       (is (= all-expected-keys-no-control
              (set (keys (aou/make-inputs cromwell-url
